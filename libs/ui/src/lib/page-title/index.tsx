46e13513f6393a81eea1d0bdad5ba9e349e2cc1e
Pooria Mehregan <pooriamehregan@gmail.com>
2023-03-14 14:08:24 +0100

    feat: add page title and subtitle

---
+++ libs/ui/src/lib/page-title/index.tsx
@@ -0,0 +1,23 @@
+import {Colour, theme} from '@fellesdatakatalog/theme';
+import styled from 'styled-components';
+
+/* eslint-disable-next-line */
+export interface PageTitleProps {
+  children: React.ReactNode;
+}
+
+const StyledPageTitle = styled.div`
+  width: 100%;
+  height: ${theme.spacing('S32')};
+  font-style: normal;
+  font-weight: ${theme.fontWeight('FW700')};
+  font-size: ${theme.fontSize('FS28')};
+  line-height: ${theme.spacing('S16')};
+  color: ${theme.colour(Colour.NEUTRAL, 'N60')};
+`;
+
+export function PageTitle({children}: PageTitleProps) {
+  return <StyledPageTitle>{children}</StyledPageTitle>;
+}
+
+export default PageTitle;
