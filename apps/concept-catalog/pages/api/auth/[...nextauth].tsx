f25dbb9a85431943a9970922b97dc7debbef9586
Jeff Reiffers <jeffreiffers@gmail.com>
2023-04-17 09:54:20 +0200

    feat: api to get concepts for catalog

--- apps/concept-catalog/pages/api/auth/[...nextauth].tsx
+++ apps/concept-catalog/pages/api/auth/[...nextauth].tsx
@@ -1,16 +1,7 @@
 import NextAuth from 'next-auth';
 import KeycloakProvider from 'next-auth/providers/keycloak';
-import {signIn, useSession} from 'next-auth/react';
-import {useEffect} from 'react';
-
-export {SessionProvider, useSession, signIn, signOut} from 'next-auth/react';
-export type {Session} from 'next-auth';
 
 export const authOptions = {
-  pages: {
-    signIn: '/auth/signin',
-    signOut: '/auth/signout',
-  },
   providers: [
     KeycloakProvider({
       clientId: process.env.KEYCLOAK_ID ?? '',
@@ -18,23 +9,23 @@ export const authOptions = {
       issuer: process.env.KEYCLOAK_ISSUER,
       idToken: true,
       profile(profile, tokens) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email ?? null,
-          image: profile.email ?? null,
-          // Append the id token to the profile
-          idToken: tokens.id_token,
-        };
-      },
+          return {
+            id: profile.sub,
+            name: profile.name,
+            email: profile.email ?? null,
+            image: profile.email ?? null,
+            // Append the id token to the profile
+            idToken: tokens.id_token,
+          };
+        },
     }),
   ],
   callbacks: {
     async session({session, token}: any) {
       session.user = {
         ...{
-          // append the id token to the next-auth session
-          idToken: token.idToken ?? null,
+            // append the id token to the next-auth session
+            idToken: token.idToken ?? null,
         },
         ...{
           id: session.user.id ?? null,
@@ -45,9 +36,8 @@ export const authOptions = {
       };
       return session;
     },
-    async jwt({token, user}: any) {
+    async jwt({token, user, account}) {
       if (user) {
-        // append the id token to the next-auth token
         token.idToken = user.idToken;
       }
       return token;
