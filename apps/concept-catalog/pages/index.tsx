3f1def114ac56beb7521502c15230016212585f0
Jeff Reiffers <jeffreiffers@gmail.com>
2023-05-12 09:00:05 +0200

    chore: redirect to registration portal on root

---
+++ apps/concept-catalog/pages/index.tsx
@@ -0,0 +1,14 @@
+export const HomePage = () => {
+  return <></>;
+};
+
+export async function getServerSideProps() {
+  return {
+    redirect: {
+      permanent: false,
+      destination: process.env.FDK_REGISTRATION_BASE_URI,
+    }
+  }
+}
+
+export default HomePage;
