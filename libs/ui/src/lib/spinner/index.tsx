aeba2200afd5073c0c33d5f50d1ee4a367febbe0
Jeff Reiffers <jeffreiffers@gmail.com>
2023-05-19 15:28:28 +0200

    chore: move spinner to ui lib

---
+++ libs/ui/src/lib/spinner/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { Spinner as SpinnerBase } from "@digdir/design-system-react";
+import { localization } from "@catalog-frontend/utils";
+import classes from './spinner.module.css';
+
+export const Spinner: FC = () => {
+    return (
+        <div className={classes.spinner}>
+            <SpinnerBase title={localization.loading} size="3xLarge" />
+        </div>
+    );
+}
+
+export default Spinner;
