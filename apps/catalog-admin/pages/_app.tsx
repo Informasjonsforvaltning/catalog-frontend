583bc59489bb62c32a1b4f4b38f3a9d7a5642158
Hege Aalvik <hegeaal@gmail.com>
2023-05-30 12:43:58 +0200

    feat: add static page for code lists

--- apps/catalog-admin/pages/_app.tsx
+++ apps/catalog-admin/pages/_app.tsx
@@ -1,20 +1,22 @@
-import {FC} from 'react';
-import {AppProps} from 'next/app';
+import { FC } from 'react';
+import { AppProps } from 'next/app';
 import Head from 'next/head';
-import {localization} from '@catalog-frontend/utils';
-import type {Session} from 'next-auth';
-import {SessionProvider} from 'next-auth/react';
-import {Layout} from '@catalog-frontend/ui';
+import { localization } from '@catalog-frontend/utils';
+import type { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { Layout } from '@catalog-frontend/ui';
+import '@altinn/figma-design-tokens/dist/tokens.css';
+import '@digdir/design-system-tokens/brand/digdir/tokens.css';
 
-const CustomApp: FC<AppProps<{session: Session}>> = ({
-  Component,
-  pageProps: {session, ...pageProps},
-}) => {
+const CustomApp: FC<AppProps<{ session: Session }>> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
       <Head>
         <title>{localization.catalogType.concept}</title>
-        <link rel='shortcut icon' href='/favicon.ico' />
+        <link
+          rel='shortcut icon'
+          href='/favicon.ico'
+        />
       </Head>
       <Layout>
         <Component {...pageProps} />
--- apps/concept-catalog/pages/_app.tsx
+++ apps/concept-catalog/pages/_app.tsx
@@ -3,6 +3,8 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import type { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+import '@altinn/figma-design-tokens/dist/tokens.css';
+import '@digdir/design-system-tokens/brand/digdir/tokens.css';
 import { localization } from '@catalog-frontend/utils';
 import { Layout } from '@catalog-frontend/ui';
 import { QueryClient, QueryClientProvider } from 'react-query';
