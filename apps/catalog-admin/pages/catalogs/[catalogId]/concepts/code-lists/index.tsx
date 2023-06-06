bdda0401ac83245013ae2ba33b7b4df4f0fe9d0e
Hege Aalvik <48259307+hegeaal@users.noreply.github.com>
2023-06-06 08:54:51 +0200

    Feat/ha/static codelists page (#87)

--- apps/catalog-admin/pages/catalogs/[catalogId]/concepts/code-lists/index.tsx
+++ apps/catalog-admin/pages/catalogs/[catalogId]/concepts/code-lists/index.tsx
@@ -1,8 +1,57 @@
 import React from 'react';
 import styles from './code-lists.module.css';
+import { Button, Accordion, Label, Ingress } from '@digdir/design-system-react';
+import { SearchField } from '@catalog-frontend/ui';
+import { PlusCircleIcon, FileImportIcon } from '@navikt/aksel-icons';
+import data from './mock-data.json';
 
 export const CodeListsPage = () => {
-  return <h1>Code lists</h1>;
+  return (
+    <div className={styles.center}>
+      <div className={styles.page}>
+        <div className={styles.row}>
+          <SearchField
+            ariaLabel={''}
+            placeholder='Søk etter kodeliste...'
+          />
+          <div className={styles.buttons}>
+            <div className={styles.buttons}>
+              <Button
+                className={styles.createButton}
+                icon={<PlusCircleIcon title='a11y-title' />}
+              >
+                Opprett ny kodeliste
+              </Button>
+            </div>
+            <Button
+              className={styles.importButton}
+              icon={<FileImportIcon title='a11y-title' />}
+              variant='outline'
+            >
+              Importer ny kodeliste
+            </Button>
+          </div>
+        </div>
+        <div className={styles.content}>
+          {data.map((data, index) => (
+            <Accordion
+              key={index}
+              border={true}
+              className={styles.accordion}
+            >
+              <Accordion.Item>
+                <Accordion.Header>
+                  <h1 className={styles.label}>{data.name}</h1>
+                  <p className={styles.description}> Description </p>
+                </Accordion.Header>
+                <Accordion.Content>Accordion content</Accordion.Content>
+              </Accordion.Item>
+            </Accordion>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CodeListsPage;
