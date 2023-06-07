74ab95c09e93de3e3ae6d5e4ef1379759de43066
Jeff Reiffers <jeffreiffers@gmail.com>
2023-06-07 12:13:47 +0200

    chore: add orgid validation

--- libs/data-access/src/lib/organization/api/index.ts
+++ libs/data-access/src/lib/organization/api/index.ts
@@ -1,18 +1,22 @@
 export const getOrganization = async (organizationId: string) => {
-  const resource = `${process.env.ORGANIZATION_CATALOG_BASE_URI}/organizations/${organizationId}`;
-  const options = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method: 'GET',
-    cache: 'no-cache' as RequestCache
-  };
-  const response = await fetch(resource, options)
-    .then((res) => {
-      console.log(res.text);
-      return res.status === 200 && res.json();
-    })
-    .catch((err) => console.error('getOrganization failed with: ', err));
+  if (organizationId.match(/^\d{9}$/)) {
+    const resource = `${process.env.ORGANIZATION_CATALOG_BASE_URI}/organizations/${organizationId}`;
+    const options = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+      cache: 'no-cache' as RequestCache,
+    };
+    const response = await fetch(resource, options)
+      .then((res) => {
+        console.log(res.text);
+        return res.status === 200 && res.json();
+      })
+      .catch((err) => console.error('getOrganization failed with: ', err));
 
-  return response;
+    return response;
+  }
+
+  return Promise.reject('Invalid organization id');
 };
