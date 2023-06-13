0c5fccb258fcd9045cfd2e71ddafab3ebe706c3f
Hege Aalvik <hegeaal@gmail.com>
2023-06-13 14:12:52 +0200

    feat: add validation for file upload

--- apps/catalog-admin/components/image-uploader/index.tsx
+++ apps/catalog-admin/components/image-uploader/index.tsx
@@ -1,59 +1,89 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './image-uploader.module.css';
 import { useAdminDispatch } from '../../context/admin';
 import { FileImportIcon, TrashIcon } from '@navikt/aksel-icons';
 import { localization } from '@catalog-frontend/utils';
 
 const allowedFileTypes = ['image/x-png', 'image/svg+xml'];
-export function ImageUploader() {
-  const [image, setImage] = useState<string>(undefined);
-  const [fileName, setFileName] = useState<string>(undefined);
 
-  const onImageChange = (event) => {
-    if (event.target.files && event.target.files[0]) {
-      setImage(URL.createObjectURL(event.target.files[0]));
-      setFileName(event.target.files[0].name);
+export function ImageUploader() {
+  const [image, setImage] = useState<string | undefined>(undefined);
+  const [fileName, setFileName] = useState<string | undefined>(undefined);
+  const adminDispatch = useAdminDispatch();
+
+  const onImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file && (await validateImageFile(file))) {
+      setImage(URL.createObjectURL(file));
+      setFileName(file.name);
     }
   };
 
-  const adminDispatch = useAdminDispatch();
-  useEffect(() => adminDispatch({ type: 'SET_LOGO', payload: { logo: image } }), [image]);
+  useEffect(() => {
+    adminDispatch({ type: 'SET_LOGO', payload: { logo: image } });
+  }, [image, adminDispatch]);
+
+  const validateImageFile = (file: File | null): Promise<boolean> => {
+    return new Promise((resolve) => {
+      if (!file) {
+        resolve(false);
+        return;
+      }
+      if (!file.name.match(/\.(svg|png)$/)) {
+        resolve(false);
+        return;
+      }
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const img = new Image();
+        img.onload = () => {
+          resolve(true); // Image content is valid
+        };
+        img.onerror = () => {
+          alert('The content of the file is not valid.');
+          resolve(false);
+        };
+        img.src = e.target?.result as string;
+      };
+      reader.readAsDataURL(file);
+    });
+  };
+
+  const resetImage = () => {
+    setFileName(undefined);
+    setImage(undefined);
+  };
 
   return (
-    <>
-      <div className={styles.container}>
-        {fileName && (
-          <div className={styles.filename}>
-            <div className={styles.file}>{fileName}</div>
-            <TrashIcon
-              title={localization.button.bin}
-              onClick={() => {
-                setFileName(undefined);
-                setImage(undefined);
-              }}
-            />
-          </div>
-        )}
+    <div className={styles.container}>
+      {fileName && (
+        <div className={styles.filename}>
+          <div className={styles.file}>{fileName}</div>
+          <TrashIcon
+            title={localization.button.bin}
+            onClick={resetImage}
+          />
+        </div>
+      )}
 
-        <label
-          className={styles.label}
-          htmlFor='file-upload'
-          aria-haspopup='true'
-          aria-expanded='true'
-          tabIndex={0}
-        >
-          <FileImportIcon fontSize='1.5rem' />
-          <div>{localization.button.importLogo}</div>
-        </label>
-        <input
-          id='file-upload'
-          type='file'
-          accept={allowedFileTypes.join(', ')}
-          onChange={onImageChange}
-          className={styles.input}
-        />
-      </div>
-    </>
+      <label
+        className={styles.label}
+        htmlFor='file-upload'
+        aria-haspopup='true'
+        aria-expanded='true'
+        tabIndex={0}
+      >
+        <FileImportIcon fontSize='1.5rem' />
+        <div>{localization.button.importLogo}</div>
+      </label>
+      <input
+        id='file-upload'
+        type='file'
+        accept={allowedFileTypes.join(', ')}
+        onChange={onImageChange}
+        className={styles.input}
+      />
+    </div>
   );
 }
 
