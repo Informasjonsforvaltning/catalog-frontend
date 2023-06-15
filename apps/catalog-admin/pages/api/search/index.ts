2a94c437a59f6197fdcbb4d50a0b617b557d4bbf
Jeff Reiffers <jeffreiffers@gmail.com>
2023-06-15 13:22:42 +0200

    chore: change search args order

--- apps/catalog-admin/pages/api/search/index.ts
+++ apps/catalog-admin/pages/api/search/index.ts
@@ -1,20 +1,13 @@
-import {searchConceptsForCatalog} from '@catalog-frontend/data-access';
-import {SearchConceptResponse} from '@catalog-frontend/types';
-import {NextApiRequest, NextApiResponse} from 'next';
-import { getToken } from "next-auth/jwt";
+import { searchConceptsForCatalog } from '@catalog-frontend/data-access';
+import { SearchConceptResponse } from '@catalog-frontend/types';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getToken } from 'next-auth/jwt';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<SearchConceptResponse | string>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<SearchConceptResponse | string>) {
   const token = await getToken({ req });
-  const {catalogId, query: jsonSearchBody} = JSON.parse(req.body);
+  const { catalogId, query: searchQuery } = JSON.parse(req.body);
 
-  searchConceptsForCatalog(
-    catalogId,
-    `${token.access_token}`,
-    JSON.stringify(jsonSearchBody)
-  ).then(async (response) => {
+  searchConceptsForCatalog(catalogId, searchQuery, `${token.access_token}`).then(async (response) => {
     if (response?.hits) {
       return res.status(200).send(response);
     } else res.status(404).send('Not found');
