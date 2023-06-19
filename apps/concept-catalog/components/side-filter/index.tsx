4b7fb7ebbde804f0c46797b53b0eb488a917f270
Pooria Mehregan <pooriamehregan@gmail.com>
2023-06-19 10:27:05 +0200

    feat: add hashCode function

--- apps/concept-catalog/components/side-filter/index.tsx
+++ apps/concept-catalog/components/side-filter/index.tsx
@@ -2,7 +2,7 @@ import { Accordion } from '@digdir/design-system-react';
 import { memo } from 'react';
 import { action, useSearchDispatch } from '../../context/search';
 import { PublishedFilterType, StatusFilterType } from '../../context/search/state';
-import { localization as loc } from '@catalog-frontend/utils';
+import { hashCode, localization as loc } from '@catalog-frontend/utils';
 import styles from './side-filter.module.css';
 import { CheckboxGroupFilter } from './checkbox-group-filter';
 import { AccordionItem, AccordionItemProps } from './accordion-item';
@@ -25,7 +25,6 @@ const SideFilter = () => {
   ];
 
   const handleOnStatusChange = (names: string[]) => {
-    console.log(names);
     searchDispatch(
       action('SET_CONCEPT_STATUS', { filters: { status: names.map((name) => name as StatusFilterType) } }),
     );
@@ -87,9 +86,9 @@ const SideFilter = () => {
     },
   ];
 
-  const accordionItems = accordionItemContents.map((item, index) => (
+  const accordionItems = accordionItemContents.map((item) => (
     <AccordionItem
-      key={`accordion-item-${index}`}
+      key={`accordion-item-${hashCode(item.header)}`}
       {...item}
     />
   ));
