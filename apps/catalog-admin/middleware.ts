7660b32bc8679f0912222cd955ba7277206ad629
Hege Aalvik <hegeaal@gmail.com>
2023-08-04 10:29:02 +0200

    fix: build error, dynamic code evaluation not allowed in edge runtime

--- apps/catalog-admin/middleware.ts
+++ apps/catalog-admin/middleware.ts
@@ -1,8 +1,19 @@
-import { hasOrganizationAdminPermission } from '@catalog-frontend/utils';
+import jwtDecode from 'jwt-decode';
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
-function getCatalogId(url) {
+export interface ResourceRole {
+  resource: string;
+  resourceId: string;
+  role: string;
+}
+
+export interface OrganizationRole {
+  orgNr: string;
+  role: string;
+}
+
+function getCatalogId(url: string): string | null {
   const matches = url.match(/\/catalogs\/(\d+)/);
 
   if (matches && matches.length > 1) {
@@ -13,7 +24,27 @@ function getCatalogId(url) {
   }
 }
 
-export async function middleware(req) {
+const getAuthorities = (token: string): string => {
+  const tokenDecoded = jwtDecode(token);
+  return (tokenDecoded && (tokenDecoded as any).authorities) || '';
+};
+
+const getResourceRoles = (token: string): ResourceRole[] =>
+  getAuthorities(token)
+    .split(',')
+    .map((authorityDescriptor) => authorityDescriptor.split(':'))
+    .map(([resource, resourceId, role]) => ({ resource, resourceId, role }));
+
+export const hasResourceRole = (token: string, { resource, resourceId, role }: ResourceRole): boolean =>
+  getResourceRoles(token).some((r) => r.resource === resource && r.resourceId === resourceId && r.role === role);
+
+export const hasOrganizationRole = (token: string, { orgNr, role }: OrganizationRole): boolean =>
+  hasResourceRole(token, { resource: 'organization', resourceId: orgNr, role });
+
+export const hasOrganizationAdminPermission = (token: string, orgNr: string): boolean =>
+  hasOrganizationRole(token, { orgNr, role: 'admin' });
+
+export async function middleware(req: any) {
   const catalogId = getCatalogId(req.nextUrl.pathname);
 
   if (catalogId) {
