e50050efbc7c3c532e643449dbc27c2ccc4d7b2e
Jeff Reiffers <jeffreiffers@gmail.com>
2023-09-25 14:02:17 +0200

    fix: use inter font

--- apps/concept-catalog/pages/_app.tsx
+++ apps/concept-catalog/pages/_app.tsx
@@ -7,12 +7,12 @@ import '@altinn/figma-design-tokens/dist/tokens.css';
 import '@digdir/design-system-tokens/brand/digdir/tokens.css';
 import { localization } from '@catalog-frontend/utils';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Heebo } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import { SearchContextProvider } from '../context/search';
 import { CatalogDesignContextProvider } from '../context/catalog-design';
 import CatalogLayout from '../components/catalog-layout';
 
-const heebo = Heebo({ subsets: ['latin'] });
+const font = Inter({ subsets: ['latin'] });
 
 const CustomApp: FC<AppProps<{ session: Session }>> = ({ Component, pageProps: { session, ...pageProps } }) => {
   const queryClient = new QueryClient();
@@ -30,7 +30,7 @@ const CustomApp: FC<AppProps<{ session: Session }>> = ({ Component, pageProps: {
               />
             </Head>
 
-            <CatalogLayout className={heebo.className}>
+            <CatalogLayout className={font.className}>
               <Component {...pageProps} />
             </CatalogLayout>
           </CatalogDesignContextProvider>
