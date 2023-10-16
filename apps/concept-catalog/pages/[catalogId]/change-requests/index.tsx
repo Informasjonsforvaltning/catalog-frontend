0b23692c887b362991ad5ea707298fddcfc33bf5
terjesyl <108797958+terjesyl@users.noreply.github.com>
2023-10-16 15:50:30 +0200

    fix(concept:changeRequests): add link to change request listitems, refactor

--- apps/concept-catalog/pages/[catalogId]/change-requests/index.tsx
+++ apps/concept-catalog/pages/[catalogId]/change-requests/index.tsx
@@ -7,13 +7,12 @@ import {
   getTranslateText,
   hasOrganizationReadPermission,
   localization as loc,
-  validChangeRequestId,
   validOrganizationNumber,
   validUUID,
 } from '@catalog-frontend/utils';
 import styles from './change-requests-page.module.css';
 import { useRouter } from 'next/router';
-import { Heading } from '@digdir/design-system-react';
+import { Heading, Link } from '@digdir/design-system-react';
 import { Session, getServerSession } from 'next-auth';
 import { authOptions } from '../../api/auth/[...nextauth]';
 
@@ -25,17 +24,6 @@ export const ChangeRequestsPage = ({ organization, changeRequests, conceptsWithC
     const catalogId = router.query.catalogId.toString();
     if (validOrganizationNumber(catalogId)) {
       router.push(`/${catalogId}/change-requests/new`);
-    }
-  };
-
-  const handleListItemClick = ({ id: changeRequestId }) => {
-    const catalogId = router.query.catalogId.toString();
-    if (
-      validOrganizationNumber(catalogId) &&
-      validUUID(changeRequestId) &&
-      validChangeRequestId(changeRequests, changeRequestId)
-    ) {
-      router.push(`/${catalogId}/change-requests/${changeRequestId}`);
     }
   };
 
@@ -57,27 +45,41 @@ export const ChangeRequestsPage = ({ organization, changeRequests, conceptsWithC
         </Heading>
         <div className={styles.listWrapper}>
           <ul className={styles.list}>
-            {changeRequests.map(({ id, catalogId, conceptId, timeForProposal, proposedBy, status }) => (
+            {changeRequests.map(({ id, title, catalogId, conceptId, timeForProposal, proposedBy, status }) => (
               <li
                 key={id}
                 itemID={id}
-                title={catalogId}
                 className={styles.listItem}
-                onClick={() => handleListItemClick(id)}
               >
                 <div className={styles.listContent}>
                   <div>
-                    <h2 className={styles.heading}>
-                      {conceptId && conceptsWithChangeRequest
-                        ? getTranslateText(
-                            conceptsWithChangeRequest?.hits?.find((concept) => concept.originaltBegrep === conceptId)
-                              ?.anbefaltTerm?.navn,
-                          )
-                        : loc.suggestionForNewConcept}
-                    </h2>
+                    <Link
+                      href={
+                        validOrganizationNumber(catalogId) &&
+                        validUUID(id) &&
+                        changeRequests.find(({ id: changeRequestId }) => changeRequestId === id)
+                          ? `/${catalogId}/change-requests/${id}`
+                          : '#'
+                      }
+                    >
+                      <Heading
+                        level={3}
+                        size='xsmall'
+                        className={styles.heading}
+                      >
+                        {title ||
+                          (conceptId && conceptsWithChangeRequest
+                            ? getTranslateText(
+                                conceptsWithChangeRequest?.hits?.find(
+                                  (concept) => concept.originaltBegrep === conceptId,
+                                )?.anbefaltTerm?.navn,
+                              )
+                            : loc.suggestionForNewConcept)}
+                      </Heading>
+                    </Link>
                     <div className={styles.text}>
-                      <p className={styles.time}>{convertTimestampToDateAndTime(timeForProposal)}</p>
-                      <p className={styles.proposedBy}>
+                      <p>{convertTimestampToDateAndTime(timeForProposal)}</p>
+                      <p>
                         {proposedBy.name
                           .split(' ')
                           .map((namePart) => capitalizeFirstLetter(namePart))
