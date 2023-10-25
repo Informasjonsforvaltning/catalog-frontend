d1d4d4a6e0490535b7423aad4530645579fcaf61
Hege Aalvik <hegeaal@gmail.com>
2023-10-25 09:35:31 +0200

    feat: init

---
+++ apps/service-catalog/app/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.styled-components file.
+   */
+  return (
+    <StyledPage>
+      <h1>Service Catalog</h1>
+    </StyledPage>
+  );
+}
