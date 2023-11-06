32f4ad65024ea85ee61ec823e16c685f19282a7c
Hege Aalvik <hegeaal@gmail.com>
2023-11-06 15:42:05 +0100

    chore: update yarn.lock

--- apps/service-catalog/app/page.tsx
+++ apps/service-catalog/app/page.tsx
@@ -1,12 +1,5 @@
 'use client';
 
-import styled from 'styled-components';
-
-const StyledPage = styled.div`
-  .page {
-  }
-`;
-
 export default async function Index() {
   /*
    * Replace the elements below with your own.
@@ -14,8 +7,8 @@ export default async function Index() {
    * Note: The corresponding styles are in the ./index.styled-components file.
    */
   return (
-    <StyledPage>
+    <div>
       <h1>Service Catalog</h1>
-    </StyledPage>
+    </div>
   );
 }
