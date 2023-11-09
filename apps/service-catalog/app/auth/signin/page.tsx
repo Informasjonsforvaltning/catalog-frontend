3a3d09576a54068941c104703876488b668e4c46
Hege Aalvik <hegeaal@gmail.com>
2023-11-09 13:33:44 +0100

    feat: add layout and authentication to service-catalog

---
+++ apps/service-catalog/app/auth/signin/page.tsx
@@ -0,0 +1,30 @@
+import { BreadcrumbType, Breadcrumbs, CenterContainer, PageBanner } from '@catalog-frontend/ui';
+import { localization } from '@catalog-frontend/utils';
+import Heading from './heading';
+
+export const SignIn = () => {
+  const breadcrumbList = [
+    {
+      href: `#`,
+      text: localization.auth.login,
+    },
+  ] as BreadcrumbType[];
+
+  return (
+    <>
+      <Breadcrumbs
+        baseURI={process.env.FDK_REGISTRATION_BASE_URI}
+        breadcrumbList={breadcrumbList}
+      />
+      <PageBanner
+        title={localization.catalogType.concept}
+        subtitle={localization.auth.login}
+      />
+      <CenterContainer>
+        <Heading />
+      </CenterContainer>
+    </>
+  );
+};
+
+export default SignIn;
--- apps/service-catalog/app/page.tsx
+++ apps/service-catalog/app/page.tsx
@@ -1,11 +1,4 @@
-'use client';
-
 export default async function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.styled-components file.
-   */
   return (
     <div>
       <h1>Service Catalog</h1>
