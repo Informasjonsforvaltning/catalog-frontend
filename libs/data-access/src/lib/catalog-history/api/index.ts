ef56f1da4b8885ac1d7dc173de9708a252bcff4f
Jeff Reiffers <jeffreiffers@gmail.com>
2023-11-14 09:42:51 +0100

    feat (concept): add concept history paging

--- libs/data-access/src/lib/catalog-history/api/index.ts
+++ libs/data-access/src/lib/catalog-history/api/index.ts
@@ -1,5 +1,5 @@
-export const getHistory = async (catalogId: string, resourceId: string, accessToken: string) => {
-  const resource = `${process.env.CATALOG_HISTORY_SERVICE_BASE_URI}/${catalogId}/${resourceId}/updates`;
+export const getHistory = async (catalogId: string, resourceId: string, accessToken: string, page = 1, size = 10) => {
+  const resource = `${process.env.CATALOG_HISTORY_SERVICE_BASE_URI}/${catalogId}/${resourceId}/updates?page=${page}&size=${size}`;
   const options = {
     headers: {
       Authorization: `Bearer ${accessToken}`,
