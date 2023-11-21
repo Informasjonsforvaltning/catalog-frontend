b91d075b47cf7267dcddf76856ac2290bd55055b
Hege Aalvik <hegeaal@gmail.com>
2023-11-21 15:50:21 +0100

    feat: add form for service

--- apps/service-catalog/app/actions/public-services/actions.ts
+++ apps/service-catalog/app/actions/public-services/actions.ts
@@ -1,8 +1,11 @@
 'use server';
 
-import { getAllPublicServices } from '@catalog-frontend/data-access';
+import { createPublicService, getAllPublicServices } from '@catalog-frontend/data-access';
+import { ServiceToBeCreated } from '@catalog-frontend/types';
 import { authOptions, validateSession } from '@catalog-frontend/utils';
 import { getServerSession } from 'next-auth';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function getPublicServices(catalogId: string) {
   const session = await getServerSession(authOptions);
@@ -16,5 +19,35 @@ export async function getPublicServices(catalogId: string) {
     return jsonResponse;
   } catch (error) {
     return;
+  }
+}
+
+export async function createNewPublicService(catalogId: string, formData: FormData) {
+  const newPublicService: ServiceToBeCreated = {
+    catalogId: catalogId,
+    title: {
+      nb: formData.get('title.nb'),
+    },
+    description: {
+      nb: formData.get('description.nb'),
+    },
+  };
+
+  const session = await getServerSession(authOptions);
+  await validateSession(session);
+  let success = false;
+  try {
+    const response = await createPublicService(newPublicService, catalogId, `${session?.accessToken}`);
+    if (response.status !== 201) {
+      throw new Error();
+    }
+    success = true;
+  } catch (error) {
+    return;
+  } finally {
+    if (success) {
+      revalidatePath(`/catalogs/${catalogId}/public-services`);
+      redirect(`/catalogs/${catalogId}/public-services`);
+    }
   }
 }
--- libs/data-access/src/lib/public-services/api/index.tsx
+++ libs/data-access/src/lib/public-services/api/index.tsx
@@ -1,3 +1,5 @@
+import { Service } from '@catalog-frontend/types';
+
 const path = `${process.env.SERVICE_CATALOG_BASE_URI}`;
 
 export const getAllPublicServices = async (catalogId: string, accessToken: string) => {
@@ -7,6 +9,19 @@ export const getAllPublicServices = async (catalogId: string, accessToken: strin
       Authorization: `Bearer ${accessToken}`,
       'Content-Type': 'application/json',
     },
+  };
+  return await fetch(resource, options);
+};
+
+export const createPublicService = async (publicService: Partial<Service>, catalogId: string, accessToken: string) => {
+  const resource = `${path}/catalogs/${catalogId}/public-services`;
+  const options = {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(publicService),
   };
   return await fetch(resource, options);
 };
