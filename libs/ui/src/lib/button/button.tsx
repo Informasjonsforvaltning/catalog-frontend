196f5167399ed557aff6fd09a125b1efcfd5bd6c
Jeff Reiffers <jeffreiffers@gmail.com>
2023-11-27 10:04:42 +0100

    fix: use size small and use value for language

--- libs/ui/src/lib/button/button.tsx
+++ libs/ui/src/lib/button/button.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { Button as DigdirButton } from '@digdir/design-system-react';
+import { Button as DigdirButton, ButtonProps } from '@digdir/design-system-react';
 import cn from './button.module.css';
 
-export const Button = ({ children, ...props }: any) => (
+export const Button = ({ children, ...props }: ButtonProps) => (
   <DigdirButton
     {...props}
     className={cn.button}
+    size={props.size ?? 'small'}
   >
     {children}
   </DigdirButton>
