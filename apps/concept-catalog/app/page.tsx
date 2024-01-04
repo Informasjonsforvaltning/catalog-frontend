2a0f7c8ec1bec24bdd6544844e1470e8da8d34f3
Jeff Reiffers <jeffreiffers@gmail.com>
2024-01-04 08:31:54 +0100

    fix (concept): opt in dynamic rendering

--- apps/concept-catalog/app/page.tsx
+++ apps/concept-catalog/app/page.tsx
@@ -1,6 +1,9 @@
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 const Home = async () => {
+  // NOTICE: Call cookies() to opt into dynamic rendering
+  cookies();
   redirect(`${process.env.FDK_REGISTRATION_BASE_URI}`);
 };
 
