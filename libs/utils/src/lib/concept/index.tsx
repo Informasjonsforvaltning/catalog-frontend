bb723cd9c160ecd3e6264071c9918bdabbc104e6
Jeff Reiffers <jeffreiffers@gmail.com>
2024-01-15 15:36:16 +0100

    fix(concept): handle empty values on save

--- libs/utils/src/lib/concept/index.tsx
+++ libs/utils/src/lib/concept/index.tsx
@@ -21,3 +21,29 @@ export const updateDefinitionsIfEgendefinert = (cr: Concept): Concept => {
     definisjonForSpesialister: removeSourceIfEgendefinert(cr.definisjonForSpesialister),
   };
 };
+
+export const pruneEmptyProperties = (obj: any, reduceAsArray = false): any => {
+  if (!obj) {
+    return null;
+  }
+  const filteredKeys = Object.keys(obj).filter(
+    (key) => obj[key] != null && obj[key] !== '' && (Array.isArray(obj[key]) ? obj[key].length !== 0 : true),
+  );
+
+  return reduceAsArray
+    ? filteredKeys.reduce((acc, key) => {
+        if (typeof obj[key] === 'object') {
+          const prunedObject = pruneEmptyProperties(obj[key]);
+          return Object.keys(prunedObject).length === 0 ? acc : [...acc, prunedObject];
+        }
+        return [...acc, obj[key]];
+      }, [] as any[])
+    : filteredKeys.reduce((acc, key) => {
+        if (typeof obj[key] === 'object') {
+          const isArray = Array.isArray(obj[key]);
+          const prunedObject = pruneEmptyProperties(obj[key], isArray);
+          return Object.keys(prunedObject).length === 0 ? acc : { ...acc, [key]: prunedObject };
+        }
+        return { ...acc, [key]: obj[key] };
+      }, {});
+};
