c9c90d1c66b4e8ce16023b985b9d9fc03fc7784f
Jeff Reiffers <jeffreiffers@gmail.com>
2024-04-05 14:21:58 +0200

    chore: some code improvments for next-auth authOptions and use trigger update for updating token

--- libs/utils/src/lib/auth/auth-options/index.ts
+++ libs/utils/src/lib/auth/auth-options/index.ts
@@ -1,6 +1,45 @@
+import { AuthOptions } from 'next-auth';
 import KeycloakProvider from 'next-auth/providers/keycloak';
 
-export const authOptions = {
+const isAfterNow = (date: number) => {
+  return Date.now() < date * 1000;
+};
+
+const refreshToken = async (token: any) => {
+  // If the access token has expired, try to refresh it
+  try {
+    // https://accounts.google.com/.well-known/openid-configuration
+    // We need the `token_endpoint`.
+    const response = await fetch(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        client_id: `${process.env.KEYCLOAK_ID}`,
+        client_secret: `${process.env.KEYCLOAK_SECRET}`,
+        grant_type: 'refresh_token',
+        refresh_token: `${token.refresh_token}`,
+      }),
+      method: 'POST',
+    });
+
+    const tokens = await response.json();
+
+    if (!response.ok) throw tokens;
+
+    return {
+      ...token, // Keep the previous token properties
+      access_token: tokens.access_token,
+      expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
+      // Fall back to old refresh token, but note that
+      // many providers may only allow using a refresh token once.
+      refresh_token: tokens.refresh_token ?? token.refresh_token,
+    };
+  } catch (error) {
+    // The error property will be used client-side to handle the refresh token error
+    return { ...token, error: 'RefreshAccessTokenError' as const };
+  }
+};
+
+export const authOptions: AuthOptions = {
   providers: [
     KeycloakProvider({
       clientId: process.env.KEYCLOAK_ID ?? '',
@@ -33,52 +72,25 @@ export const authOptions = {
 
       return session;
     },
-    async jwt({ token, user, account }) {
+    async jwt({ token, user, account, trigger }) {
+      if (trigger === 'update') {
+        return refreshToken(token);
+      }
+
       if (account) {
         // Save the access token and refresh token in the JWT on the initial login
         return {
           ...user,
           access_token: account.access_token,
-          expires_at: Math.floor(Date.now() / 1000 + account.expires_in),
+          expires_at: Math.floor(Date.now() / 1000 + Number(account.expires_in)),
           refresh_token: account.refresh_token,
           id_token: account.id_token,
         };
-      } else if (Date.now() < token.expires_at * 1000) {
+      } else if (isAfterNow(Number(token.expires_at))) {
         // If the access token has not expired yet, return it
         return token;
       } else {
-        // If the access token has expired, try to refresh it
-        try {
-          // https://accounts.google.com/.well-known/openid-configuration
-          // We need the `token_endpoint`.
-          const response = await fetch(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, {
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams({
-              client_id: process.env.KEYCLOAK_ID,
-              client_secret: process.env.KEYCLOAK_SECRET,
-              grant_type: 'refresh_token',
-              refresh_token: token.refresh_token,
-            }),
-            method: 'POST',
-          });
-
-          const tokens = await response.json();
-
-          if (!response.ok) throw tokens;
-
-          return {
-            ...token, // Keep the previous token properties
-            access_token: tokens.access_token,
-            expires_at: Math.floor(Date.now() / 1000 + tokens.expires_in),
-            // Fall back to old refresh token, but note that
-            // many providers may only allow using a refresh token once.
-            refresh_token: tokens.refresh_token ?? token.refresh_token,
-          };
-        } catch (error) {
-          console.error('Error refreshing access token', error);
-          // The error property will be used client-side to handle the refresh token error
-          return { ...token, error: 'RefreshAccessTokenError' as const };
-        }
+        return refreshToken(token);
       }
     },
   },
