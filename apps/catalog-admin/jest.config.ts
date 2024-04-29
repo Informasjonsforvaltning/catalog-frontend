3bc93f42db4951fb6dd279f17a636c9917ac13bf
Jeff Reiffers <jeffreiffers@gmail.com>
2024-04-29 12:08:37 +0200

    fix: css test error

--- apps/catalog-admin/jest.config.ts
+++ apps/catalog-admin/jest.config.ts
@@ -2,10 +2,14 @@
 export default {
   displayName: 'catalog-admin',
   preset: '../../jest.preset.js',
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+    '@digdir/designsystemet-(theme|css)$': '<rootDir>/__mocks__/styleMock.js',
+  },
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }]
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/catalog-admin'
+  coverageDirectory: '../../coverage/apps/catalog-admin',
 };
