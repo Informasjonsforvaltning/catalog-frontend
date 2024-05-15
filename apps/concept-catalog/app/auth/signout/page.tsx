6b50ca971a95352953a23d3b81989fea5513b761
Jeff Reiffers <jeffreiffers@gmail.com>
2024-05-15 06:58:45 +0200

    fix: use window.location instead of route

--- apps/concept-catalog/app/auth/signout/page.tsx
+++ apps/concept-catalog/app/auth/signout/page.tsx
@@ -4,17 +4,14 @@ import { CenterContainer, PageBanner, Spinner } from '@catalog-frontend/ui';
 import { localization } from '@catalog-frontend/utils';
 import { Heading } from '@digdir/designsystemet-react';
 import { signOut } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export const SignOut = () => {
-  const router = useRouter();
-
   useEffect(() => {
-    signOut({ redirect: false }).then(() => {
-      router.push('/');
+    signOut({ redirect: false, callbackUrl: '/' }).then(({ url }) => {
+      window.location.href = url;
     });
-  }, [router]);
+  }, []);
 
   return (
     <>
--- libs/ui/src/lib/keycloak-signin/index.tsx
+++ libs/ui/src/lib/keycloak-signin/index.tsx
@@ -4,14 +4,15 @@ import { localization } from '@catalog-frontend/utils';
 import { signIn } from 'next-auth/react';
 import { useSearchParams } from 'next/navigation';
 import { Spinner } from '../spinner';
+import { useEffect } from 'react';
 
 const KeycloakSignin = () => {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl') ?? '/';
 
-  if (typeof window !== 'undefined') {
+  useEffect(() => {
     signIn('keycloak', { callbackUrl });
-  }
+  }, [callbackUrl]);
 
   return <Spinner title={localization.auth.loggingIn} />;
 };
