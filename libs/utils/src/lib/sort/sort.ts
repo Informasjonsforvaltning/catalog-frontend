6e95a1bfd99fa9daa2da700927ccb621c6e41d5a
hegeaal <hegeaal@gmail.com>
2024-08-13 17:09:30 +0200

    feat: add sorting to datasets search hits

--- libs/utils/src/lib/sort/sort.ts
+++ libs/utils/src/lib/sort/sort.ts
@@ -1,13 +1,19 @@
-export const sortAscending = (a: any, b: any) => {
-  if (a === b) {
-    return 0;
-  }
-  return a > b ? 1 : -1;
+// For sorting strings in ascending order (case-insensitive)
+export const sortAscending = (a: string, b: string) => {
+  return a.toLowerCase().localeCompare(b.toLowerCase());
 };
 
-export const sortDescending = (a: any, b: any) => {
-  if (a === b) {
-    return 0;
-  }
-  return a < b ? 1 : -1;
+// For sorting strings in descending order (case-insensitive)
+export const sortDescending = (a: string, b: string) => {
+  return b.toLowerCase().localeCompare(a.toLowerCase());
+};
+
+// For sorting ISO 8601 date strings in ascending order
+export const sortDateStringsAscending = (a: string, b: string) => {
+  return new Date(a).getTime() - new Date(b).getTime();
+};
+
+// For sorting ISO 8601 date strings in descending order
+export const sortDateStringsDescending = (a: string, b: string) => {
+  return new Date(b).getTime() - new Date(a).getTime();
 };
