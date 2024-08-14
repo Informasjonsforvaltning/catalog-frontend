19b39c48e30ca584f3f7d1c1fb5300cef21e419d
hegeaal <hegeaal@gmail.com>
2024-08-14 12:32:15 +0200

    feat: add filter for status dataset page

--- apps/dataset-catalog/app/catalogs/[catalogId]/datasets/page.tsx
+++ apps/dataset-catalog/app/catalogs/[catalogId]/datasets/page.tsx
@@ -1,4 +1,4 @@
-import { Dataset, Organization, ReferenceDataCode, Service } from '@catalog-frontend/types';
+import { Dataset, Organization } from '@catalog-frontend/types';
 import { BreadcrumbType, Breadcrumbs, PageBanner } from '@catalog-frontend/ui';
 import {
   getTranslateText,
@@ -8,21 +8,17 @@ import {
 } from '@catalog-frontend/utils';
 
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
-import { getAdmsStatuses, getOrganization } from '@catalog-frontend/data-access';
-import ServicePageClient from './datasets-page-client';
-import { getDatasets } from 'apps/dataset-catalog/app/actions/actions';
+import { getOrganization } from '@catalog-frontend/data-access';
+import { getDatasets } from '../../../actions/actions';
 import DatasetsPageClient from './datasets-page-client';
 
-export default async function ServiceSearchHitsPage({ params }: Params) {
+export default async function DatasetSearchHitsPage({ params }: Params) {
   const { catalogId } = params;
-  const session = await getValidSession({ callbackUrl: `/catalogs/${catalogId}/services` });
+  const session = await getValidSession({ callbackUrl: `/catalogs/${catalogId}/datasets` });
 
   const datasets: Dataset[] = await getDatasets(catalogId);
   const organization: Organization = await getOrganization(catalogId).then((res) => res.json());
   const hasWritePermission = await hasOrganizationWritePermission(session.accessToken, catalogId);
-
-  const statusesResponse = await getAdmsStatuses().then((res) => res.json());
-  const statuses: ReferenceDataCode[] = statusesResponse.statuses;
 
   const breadcrumbList = [
     {
@@ -41,8 +37,6 @@ export default async function ServiceSearchHitsPage({ params }: Params) {
       <DatasetsPageClient
         datasets={datasets}
         hasWritePermission={hasWritePermission}
-        statuses={statuses}
-        catalogId={catalogId}
       />
     </>
   );
