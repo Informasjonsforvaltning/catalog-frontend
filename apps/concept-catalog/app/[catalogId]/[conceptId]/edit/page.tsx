3591868f0a6127ea0b8a6d1c727a9d0f357a107a
Jeff Reiffers <jeffreiffers@gmail.com>
2024-08-21 09:52:22 +0200

    fix: verify if concept belongs to catalog

--- apps/concept-catalog/app/[catalogId]/[conceptId]/edit/page.tsx
+++ apps/concept-catalog/app/[catalogId]/[conceptId]/edit/page.tsx
@@ -1,9 +1,16 @@
+import { getConcept } from '@catalog-frontend/data-access';
 import { withWriteProtectedPage } from '../../../../utils/auth';
-import { redirect } from 'next/navigation';
+import { redirect, RedirectType } from 'next/navigation';
 
 export default withWriteProtectedPage(
   ({ catalogId, conceptId }) => `/${catalogId}/${conceptId}/edit`,
-  async ({ catalogId, conceptId }) => {
+  async ({ catalogId, conceptId, session }) => {
+    const concept = await getConcept(`${conceptId}`, `${session?.accessToken}`).then((response) => {
+      if (response.ok) return response.json();
+    });
+    if (!concept || concept.ansvarligVirksomhet?.id !== catalogId) {
+      return redirect(`/notfound`, RedirectType.replace);
+    }
     redirect(`${process.env.CONCEPT_CATALOG_GUI_BASE_URI}/${catalogId}/${conceptId}`);
   },
 );
