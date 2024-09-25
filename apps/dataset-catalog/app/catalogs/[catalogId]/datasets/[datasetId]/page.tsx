2dd93e5df0c885e5bcb644ffc13c4b782c80eb4b
hegeaal <hegeaal@gmail.com>
2024-09-25 09:23:04 +0200

    feat: add banner dataset-catalog

--- apps/dataset-catalog/app/catalogs/[catalogId]/datasets/[datasetId]/page.tsx
+++ apps/dataset-catalog/app/catalogs/[catalogId]/datasets/[datasetId]/page.tsx
@@ -1,11 +1,14 @@
-import { Breadcrumbs, BreadcrumbType } from '@catalog-frontend/ui';
+import { Breadcrumbs, BreadcrumbType, PageBanner } from '@catalog-frontend/ui';
 import { getDatasetById } from '../../../../actions/actions';
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 import { getTranslateText, localization } from '@catalog-frontend/utils';
+import { Organization } from '@catalog-frontend/types';
+import { getOrganization } from '@catalog-frontend/data-access';
 
 export default async function EditDatasetPage({ params }: Params) {
   const { catalogId, datasetId } = params;
   const dataset = await getDatasetById(catalogId, datasetId);
+  const organization: Organization = await getOrganization(catalogId).then((res) => res.json());
 
   const breadcrumbList = [
     {
@@ -21,6 +24,10 @@ export default async function EditDatasetPage({ params }: Params) {
   return (
     <>
       <Breadcrumbs breadcrumbList={breadcrumbList} />
+      <PageBanner
+        title={localization.catalogType.dataset}
+        subtitle={getTranslateText(organization.prefLabel).toString()}
+      />
       <div className='container'>Her kommer details-page</div>
     </>
   );
--- apps/dataset-catalog/app/catalogs/[catalogId]/datasets/series/new/page.tsx
+++ apps/dataset-catalog/app/catalogs/[catalogId]/datasets/series/new/page.tsx
@@ -1,9 +1,19 @@
-import { FormContainer } from '@catalog-frontend/ui';
+import { getOrganization } from '@catalog-frontend/data-access';
+import { Organization } from '@catalog-frontend/types';
+import { FormContainer, PageBanner } from '@catalog-frontend/ui';
+import { getTranslateText, localization } from '@catalog-frontend/utils';
 import { Textfield } from '@digdir/designsystemet-react';
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 
-export default async function NewDatasetSeriesPage() {
+export default async function NewDatasetSeriesPage({ params }: Params) {
+  const { catalogId } = params;
+  const organization: Organization = await getOrganization(catalogId).then((res) => res.json());
   return (
     <div className='container'>
+      <PageBanner
+        title={localization.catalogType.dataset}
+        subtitle={getTranslateText(organization.prefLabel).toString()}
+      />
       <FormContainer>
         <FormContainer.Header title='test' />
         <Textfield />
