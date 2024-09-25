682361c23da9f3351c9da1ea4c3db81663aea9f5
hegeaal <hegeaal@gmail.com>
2024-09-25 08:55:35 +0200

    feat: add breadcrumbs dataset-catalog

--- apps/dataset-catalog/app/catalogs/[catalogId]/datasets/[datasetId]/page.tsx
+++ apps/dataset-catalog/app/catalogs/[catalogId]/datasets/[datasetId]/page.tsx
@@ -1,14 +1,26 @@
-import { Breadcrumbs } from '@catalog-frontend/ui';
+import { Breadcrumbs, BreadcrumbType } from '@catalog-frontend/ui';
 import { getDatasetById } from '../../../../actions/actions';
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+import { getTranslateText, localization } from '@catalog-frontend/utils';
 
 export default async function EditDatasetPage({ params }: Params) {
   const { catalogId, datasetId } = params;
   const dataset = await getDatasetById(catalogId, datasetId);
 
+  const breadcrumbList = [
+    {
+      href: `/catalogs/${catalogId}/datasets`,
+      text: localization.catalogType.dataset,
+    },
+    {
+      href: `/catalogs/${catalogId}/datasets/${datasetId}`,
+      text: getTranslateText(dataset.title),
+    },
+  ] as BreadcrumbType[];
+
   return (
     <>
-      <Breadcrumbs />
+      <Breadcrumbs breadcrumbList={breadcrumbList} />
       <div className='container'>Her kommer details-page</div>
     </>
   );
