d3efdbec0ae4f81e60c5d8e181e81512c19eb103
hegeaal <hegeaal@gmail.com>
2024-09-25 08:55:35 +0200

    feat: add breadcrumbs dataset-catalog

--- apps/dataset-catalog/app/catalogs/[catalogId]/datasets/[datasetId]/edit/page.tsx
+++ apps/dataset-catalog/app/catalogs/[catalogId]/datasets/[datasetId]/edit/page.tsx
@@ -1,16 +1,32 @@
-import { Breadcrumbs } from '@catalog-frontend/ui';
+import { Breadcrumbs, BreadcrumbType } from '@catalog-frontend/ui';
 import { getDatasetById } from '../../../../../actions/actions';
 import { DatasetForm } from '../../../../../../components/dataset-form';
 
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+import { getTranslateText, localization } from '@catalog-frontend/utils';
 
 export default async function EditDatasetPage({ params }: Params) {
   const { catalogId, datasetId } = params;
   const dataset = await getDatasetById(catalogId, datasetId);
 
+  const breadcrumbList = [
+    {
+      href: `/catalogs/${catalogId}/datasets`,
+      text: localization.catalogType.dataset,
+    },
+    {
+      href: `/catalogs/${catalogId}/datasets/${datasetId}`,
+      text: getTranslateText(dataset.title),
+    },
+    {
+      href: `/catalogs/${catalogId}/datasets/${datasetId}/edit`,
+      text: localization.edit,
+    },
+  ] as BreadcrumbType[];
+
   return (
     <>
-      <Breadcrumbs />
+      <Breadcrumbs breadcrumbList={breadcrumbList} />
       <div className='container'>
         <DatasetForm
           initialValues={dataset}
--- apps/dataset-catalog/app/catalogs/[catalogId]/datasets/new/page.tsx
+++ apps/dataset-catalog/app/catalogs/[catalogId]/datasets/new/page.tsx
@@ -1,13 +1,28 @@
-import { Breadcrumbs, PageBanner } from '@catalog-frontend/ui';
+import { Breadcrumbs, BreadcrumbType, PageBanner } from '@catalog-frontend/ui';
 import { DatasetForm } from '../../../../../components/dataset-form';
 import { datasetToBeCreatedTemplate } from '../../../../../components/dataset-form/dataset-initial-values';
+import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
+import { localization } from '@catalog-frontend/utils';
 
-export default function NewDatasetPage() {
+export default async function NewDatasetPage({ params }: Params) {
   const initialValues = datasetToBeCreatedTemplate();
+
+  const { catalogId } = params;
+
+  const breadcrumbList = [
+    {
+      href: `/catalogs/${catalogId}/datasets`,
+      text: localization.catalogType.dataset,
+    },
+    {
+      href: `/catalogs/${catalogId}/datasets/new`,
+      text: localization.button.addDataset,
+    },
+  ] as BreadcrumbType[];
 
   return (
     <>
-      <Breadcrumbs />
+      <Breadcrumbs breadcrumbList={breadcrumbList} />
       <PageBanner
         title='Datasettkatalog'
         subtitle='Høgskolen for IT og arkitektur'
