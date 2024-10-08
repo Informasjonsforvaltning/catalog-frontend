9787fa5ad27db6bdb2b173d1b03492e1e176af9a
hegeaal <hegeaal@gmail.com>
2024-10-08 15:10:55 +0200

    feat: use os specific font

--- apps/catalog-admin/app/layout.tsx
+++ apps/catalog-admin/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Layout, NextAuthProvider, ReactQueryClientProvider } from '@catalog-frontend/ui';
 import { localization } from '@catalog-frontend/utils';
 import { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import './global.css';
 import { AdminContextProvider } from '../context/admin';
 
@@ -9,8 +8,6 @@ export const metadata: Metadata = {
   title: localization.catalogType.admin,
   description: localization.catalogType.admin,
 };
-
-const font = Inter({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -20,7 +17,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           <AdminContextProvider>
             <ReactQueryClientProvider>
               <Layout
-                className={font.className}
                 catalogAdminUrl={process.env.CATALOG_ADMIN_BASE_URI}
                 fdkRegistrationBaseUrl={`${process.env.CATALOG_PORTAL_BASE_URI}/catalogs`}
                 adminGuiBaseUrl={process.env.ADMIN_GUI_BASE_URI}
