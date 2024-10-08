94813d37da3ff8d5860827c4e736866a377ad949
hegeaal <hegeaal@gmail.com>
2024-10-08 15:10:55 +0200

    feat: use os specific font

--- apps/catalog-portal/app/layout.tsx
+++ apps/catalog-portal/app/layout.tsx
@@ -1,14 +1,11 @@
 import { Layout, NextAuthProvider } from '@catalog-frontend/ui';
 import { localization } from '@catalog-frontend/utils';
 import { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: localization.catalogOverview,
   description: localization.catalogOverview,
 };
-
-const font = Inter({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -16,7 +13,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <body>
         <NextAuthProvider>
           <Layout
-            className={font.className}
             catalogAdminUrl={process.env.CATALOG_ADMIN_BASE_URI}
             fdkRegistrationBaseUrl={`${process.env.CATALOG_PORTAL_BASE_URI}/catalogs`}
             adminGuiBaseUrl={process.env.ADMIN_GUI_BASE_URI}
--- apps/concept-catalog/app/layout.tsx
+++ apps/concept-catalog/app/layout.tsx
@@ -2,14 +2,11 @@ import { NextAuthProvider, ReactQueryClientProvider } from '@catalog-frontend/ui
 import { localization } from '@catalog-frontend/utils';
 import { Metadata } from 'next';
 import CatalogLayout from '../components/catalog-layout';
-import { Inter } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: localization.catalogType.concept,
   description: localization.catalogType.concept,
 };
-
-const font = Inter({ subsets: ['latin'] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -18,7 +15,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <NextAuthProvider>
           <ReactQueryClientProvider>
             <CatalogLayout
-              className={font.className}
               catalogAdminUrl={process.env.CATALOG_ADMIN_BASE_URI}
               fdkRegistrationBaseUrl={`${process.env.CATALOG_PORTAL_BASE_URI}/catalogs`}
               adminGuiBaseUrl={process.env.ADMIN_GUI_BASE_URI}
