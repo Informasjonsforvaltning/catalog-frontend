bbdae56496a7d72e1c016b174c959ef109b3b199
Jeff Reiffers <jeffreiffers@gmail.com>
2024-12-05 14:02:29 +0100

    fix: several fixes and adjustments

--- apps/concept-catalog/components/concept-form/components/relation-fieldset/index.tsx
+++ apps/concept-catalog/components/concept-form/components/relation-fieldset/index.tsx
@@ -1,36 +1,69 @@
 'use client';
 
 import { ChangeEvent, useEffect, useState } from 'react';
-import { FastField, useFormikContext } from 'formik';
+import { useFormikContext } from 'formik';
 import { Box, Combobox, Fieldset, HelpText, Paragraph, Radio, Textfield } from '@digdir/designsystemet-react';
 import { FieldsetDivider, FormikLanguageFieldset, HelpMarkdown, TitleWithTag } from '@catalog-frontend/ui';
 import { getTranslateText, localization } from '@catalog-frontend/utils';
-import { Option, UnionRelation, UnionRelationSubtypeEnum, UnionRelationTypeEnum } from '@catalog-frontend/types';
+import {
+  Option,
+  RelatedConcept,
+  UnionRelation,
+  RelationSubtypeEnum,
+  RelationTypeEnum,
+} from '@catalog-frontend/types';
 import { useSearchConcepts as useSearchInternalConcepts, useDataNorgeSearchConcepts } from '../../../../hooks/search';
 import styles from './relation-fieldset.module.scss';
 
-const relationTypes = Object.keys(UnionRelationTypeEnum)
+type RelatedConceptType = 'internal' | 'external' | 'custom';
+
+const relationTypes = Object.keys(RelationTypeEnum)
   .filter((item) => {
     return isNaN(Number(item));
   })
-  .map((key) => UnionRelationTypeEnum[key]);
+  .map((key) => RelationTypeEnum[key]);
 
-const relationSubtypes = Object.keys(UnionRelationSubtypeEnum)
+const relationSubtypes = Object.keys(RelationSubtypeEnum)
   .filter((item) => {
     return isNaN(Number(item));
   })
-  .map((key) => UnionRelationSubtypeEnum[key]);
+  .map((key) => RelationSubtypeEnum[key]);
 
-export const RelationFieldset = ({ catalogId }) => {
+type RelationFieldsetProps = {
+  catalogId: string;
+  initialRelatedConcept?: RelatedConcept;
+};
+
+const getRelatedConceptStateValue = (relatedConcept?: RelatedConcept): string[] => {
+  if (relatedConcept?.id) {
+    return [relatedConcept.id];
+  } else if (relatedConcept?.href) {
+    return [relatedConcept.href];
+  }
+  return [];
+};
+
+const getRelatedConceptTypeStateValue = (relatedConcept?: RelatedConcept): RelatedConceptType => {
+  if (relatedConcept?.custom) {
+    return 'custom';
+  } else if (relatedConcept?.id) {
+    return 'internal';
+  } else if (relatedConcept?.href) {
+    return 'external';
+  }
+  return 'internal';
+};
+
+export const RelationFieldset = ({ catalogId, initialRelatedConcept }: RelationFieldsetProps) => {
   const { errors, values, setFieldValue } = useFormikContext<UnionRelation>();
+  const [relatedConcept, setRelatedConcept] = useState<string[]>(getRelatedConceptStateValue(initialRelatedConcept));
+  const [relatedConceptType, setRelatedConceptType] = useState<RelatedConceptType>(
+    getRelatedConceptTypeStateValue(initialRelatedConcept),
+  );
   const [search, setSearch] = useState('');
-  const [relatedConcept, setRelatedConcept] = useState<string[]>([]);
-  const [relatedConceptType, setRelatedConceptType] = useState<string>('internal');
-  const [initialFetched, setInitialFetched] = useState(false);
-  const [internalEnabled, setInternalEnabled] = useState(true);
-  const [externalEnabled, setExternalEnabled] = useState(true);
+  const [searchTriggered, setSearchTriggered] = useState(false);
 
-  const { data: internalConcepts, status: internalStatus } = useSearchInternalConcepts({
+  const { data: internalConcepts } = useSearchInternalConcepts({
     catalogId,
     searchTerm: search,
     page: 0,
@@ -41,30 +74,16 @@ export const RelationFieldset = ({ catalogId }) => {
       definisjon: false,
       merknad: false,
     },
-    filters:
-      search.trim().length === 0 && values.relatertBegrep?.length
-        ? {
-            originalId: {
-              value: [values.relatertBegrep as string],
-            },
-          }
-        : undefined,
-    enabled: internalEnabled,
   });
 
-  const { data: externalConcepts, status: externalStatus } = useDataNorgeSearchConcepts({
+  const { data: externalConcepts } = useDataNorgeSearchConcepts({
     searchOperation: {
       query: search,
-      filters:
-        relatedConceptType !== 'custom' && search.trim().length === 0 && values.relatertBegrep?.length
-          ? {
-              uri: {
-                value: [values.relatertBegrep as string],
-              },
-            }
-          : undefined,
+      fields: {
+        title: true,
+      },
     },
-    enabled: externalEnabled,
+    enabled: Boolean(search),
   });
 
   const relationTypeOptions = relationTypes.map((item) => ({
@@ -78,11 +97,11 @@ export const RelationFieldset = ({ catalogId }) => {
   }));
 
   const filterRelationSubtype = (inputValue: string, option: Option) => {
-    if (values.relasjon === UnionRelationTypeEnum.GENERISK) {
-      return option.value !== UnionRelationSubtypeEnum.ER_DEL_AV && option.value !== UnionRelationSubtypeEnum.OMFATTER;
-    } else if (values.relasjon === UnionRelationTypeEnum.PARTITIV) {
+    if (values.relasjon === RelationTypeEnum.GENERISK) {
+      return option.value !== RelationSubtypeEnum.ER_DEL_AV && option.value !== RelationSubtypeEnum.OMFATTER;
+    } else if (values.relasjon === RelationTypeEnum.PARTITIV) {
       return (
-        option.value !== UnionRelationSubtypeEnum.OVERORDNET && option.value !== UnionRelationSubtypeEnum.UNDERORDNET
+        option.value !== RelationSubtypeEnum.OVERORDNET && option.value !== RelationSubtypeEnum.UNDERORDNET
       );
     }
     return true;
@@ -97,10 +116,10 @@ export const RelationFieldset = ({ catalogId }) => {
     setFieldValue('relasjonsType', value[0]);
   };
 
-  const handleSearchConceptChange = (event: ChangeEvent<HTMLInputElement>) => {    
-    setInternalEnabled(true);
-    setExternalEnabled(true);
+  const handleSearchConceptChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setRelatedConcept([]);
     setSearch(event.target.value);
+    setSearchTriggered(true);
   };
 
   const handleRelatedConceptChange = (value: string[]) => {
@@ -108,52 +127,16 @@ export const RelationFieldset = ({ catalogId }) => {
   };
 
   const handleCustomRelatedConceptChange = (e) => {
-    setInternalEnabled(false);
-    setExternalEnabled(false);
     setRelatedConcept([e.target.value]);
-  }
-  
+  };
+
   useEffect(() => {
     setFieldValue('internal', relatedConceptType === 'internal');
-    if(relatedConcept.length > 0) {
-      setRelatedConcept(() => []);
-    }
-    
   }, [relatedConceptType]);
 
   useEffect(() => {
-    setFieldValue('relatertBegrep', relatedConcept);
+    setFieldValue('relatertBegrep', relatedConcept[0]);
   }, [relatedConcept]);
-
-  useEffect(() => {
-    if (internalStatus === 'success' && externalStatus === 'success' && !initialFetched) {
-      setInitialFetched(() => true);
-      setInternalEnabled(() => false);
-      setExternalEnabled(() => false);
-
-      const selectedRelatedConceptType = internalConcepts?.hits.find((hit) => hit.id === values.relatertBegrep)
-        ? 'internal'
-        : externalConcepts?.hits.find((hit) => hit.uri === values.relatertBegrep)
-          ? 'external'
-          : 'custom';
-
-      console.log(
-        'initialFetched triggered',
-        internalConcepts,
-        externalConcepts,
-        selectedRelatedConceptType,
-        values.relatertBegrep,
-      );
-
-      setRelatedConceptType(selectedRelatedConceptType);
-      setRelatedConcept(
-        values.relatertBegrep &&
-          (selectedRelatedConceptType === 'internal' || selectedRelatedConceptType === 'external')
-          ? [values.relatertBegrep]
-          : [],
-      );
-    }
-  }, [internalStatus, externalStatus, initialFetched]);
 
   return (
     <Box className={styles.root}>
@@ -164,14 +147,13 @@ export const RelationFieldset = ({ catalogId }) => {
             <TitleWithTag
               title={
                 <>
-                  {localization.conceptForm.fieldLabel.relation}
+                  {localization.conceptForm.fieldLabel.relatedConcept}
                   <HelpMarkdown
-                    title={localization.conceptForm.fieldLabel.relation}
+                    title={localization.conceptForm.fieldLabel.relatedConcept}
                     type='button'
                     size='sm'
-                    placement='right-end'
                   >
-                    {localization.conceptForm.helpText.relation}
+                    {localization.conceptForm.helpText.relatedConcept}
                   </HelpMarkdown>
                 </>
               }
@@ -180,25 +162,115 @@ export const RelationFieldset = ({ catalogId }) => {
             />
           }
         >
-          <Combobox
+          <Radio.Group
+            legend=''
             size='sm'
-            portal={false}
-            error={errors?.relasjon}
-            value={values.relasjon ? [values.relasjon] : []}
-            onValueChange={handleRelationTypeChange}
+            value={relatedConceptType}
+            onChange={(value) => setRelatedConceptType(value as RelatedConceptType)}
           >
-            {relationTypeOptions.map((rel) => (
-              <Combobox.Option
-                key={rel.value}
-                value={rel.value}
-              >
-                {rel.label}
-              </Combobox.Option>
-            ))}
-          </Combobox>
+            <Radio value='internal'>Søk i egen katalog</Radio>
+            <Radio value='external'>Søk på data.norge.no</Radio>
+            <Radio value='custom'>Egen definert</Radio>
+          </Radio.Group>
+          {(relatedConceptType === 'external' || relatedConceptType === 'internal') && (
+            <Combobox
+              size='sm'
+              portal={false}
+              value={relatedConcept}
+              onChange={handleSearchConceptChange}
+              onValueChange={handleRelatedConceptChange}
+              error={errors.relatertBegrep}
+            >
+              <Combobox.Empty>Fant ingen treff</Combobox.Empty>
+              {relatedConceptType === 'internal' &&
+                searchTriggered &&
+                internalConcepts?.hits.map((concept) => (
+                  <Combobox.Option
+                    key={concept.originaltBegrep as string}
+                    value={concept.originaltBegrep as string}
+                  >
+                    {getTranslateText(concept.anbefaltTerm?.navn) as string}
+                  </Combobox.Option>
+                ))}
+              {relatedConceptType === 'external' &&
+                searchTriggered &&
+                externalConcepts?.hits.map((concept) => (
+                  <Combobox.Option
+                    key={concept.id as string}
+                    value={concept.uri as string}
+                    description={getTranslateText(concept.organization?.prefLabel) as string}
+                  >
+                    {getTranslateText(concept.title) as string}
+                  </Combobox.Option>
+                ))}
+              {relatedConceptType === 'internal' && !searchTriggered && initialRelatedConcept && (
+                <Combobox.Option
+                  key={initialRelatedConcept.id as string}
+                  value={initialRelatedConcept.id as string}
+                >
+                  {getTranslateText(initialRelatedConcept.title)}
+                </Combobox.Option>
+              )}
+              {relatedConceptType === 'external' && !searchTriggered && initialRelatedConcept && (
+                <Combobox.Option
+                  key={initialRelatedConcept.href as string}
+                  value={initialRelatedConcept.href as string}
+                >
+                  {getTranslateText(initialRelatedConcept.title)}
+                </Combobox.Option>
+              )}
+            </Combobox>
+          )}
+          {relatedConceptType === 'custom' && (
+            <Textfield
+              value={relatedConcept[0]}
+              onChange={handleCustomRelatedConceptChange}
+            />
+          )}
         </Fieldset>
       </Box>
-      {(values.relasjon === UnionRelationTypeEnum.GENERISK || values.relasjon === UnionRelationTypeEnum.PARTITIV) && (
+      <FieldsetDivider />
+      <Fieldset
+        size='sm'
+        legend={
+          <TitleWithTag
+            title={
+              <>
+                {localization.conceptForm.fieldLabel.relation}
+                <HelpMarkdown
+                  title={localization.conceptForm.fieldLabel.relation}
+                  type='button'
+                  size='sm'
+                  placement='right-end'
+                >
+                  {localization.conceptForm.helpText.relation}
+                </HelpMarkdown>
+              </>
+            }
+            tagColor='second'
+            tagTitle={localization.tag.required}
+          />
+        }
+      >
+        <Combobox
+          size='sm'
+          portal={false}
+          error={errors?.relasjon}
+          value={values.relasjon ? [values.relasjon] : []}
+          onValueChange={handleRelationTypeChange}
+        >
+          {relationTypeOptions.map((rel) => (
+            <Combobox.Option
+              key={rel.value}
+              value={rel.value}
+            >
+              {rel.label}
+            </Combobox.Option>
+          ))}
+        </Combobox>
+      </Fieldset>
+
+      {(values.relasjon === RelationTypeEnum.GENERISK || values.relasjon === RelationTypeEnum.PARTITIV) && (
         <>
           <Box className={styles.flex}>
             <Fieldset
@@ -269,7 +341,7 @@ export const RelationFieldset = ({ catalogId }) => {
           />
         </>
       )}
-      {values.relasjon === UnionRelationTypeEnum.ASSOSIATIV && (
+      {values.relasjon === RelationTypeEnum.ASSOSIATIV && (
         <FormikLanguageFieldset
           name='beskrivelse'
           legend={
@@ -290,76 +362,6 @@ export const RelationFieldset = ({ catalogId }) => {
           }
         />
       )}
-      <FieldsetDivider />
-      <Fieldset
-        size='sm'
-        legend={
-          <TitleWithTag
-            title={
-              <>
-                {localization.conceptForm.fieldLabel.relatedConcept}
-                <HelpMarkdown
-                  title={localization.conceptForm.fieldLabel.relatedConcept}
-                  type='button'
-                  size='sm'
-                >
-                  {localization.conceptForm.helpText.relatedConcept}
-                </HelpMarkdown>
-              </>
-            }
-            tagColor='second'
-            tagTitle={localization.tag.required}
-          />
-        }
-      >
-        <Radio.Group
-          legend=''
-          size='sm'
-          value={relatedConceptType}
-          onChange={setRelatedConceptType}
-        >
-          <Radio value='internal'>Søk i egen katalog</Radio>
-          <Radio value='external'>Søk på data.norge.no</Radio>
-          <Radio value='custom'>Egen definert</Radio>
-        </Radio.Group>
-        {(relatedConceptType === 'external' || relatedConceptType === 'internal') && (
-          <Combobox
-            size='sm'
-            portal={false}
-            value={relatedConcept}
-            onChange={handleSearchConceptChange}
-            onValueChange={handleRelatedConceptChange}
-            error={errors.relatertBegrep}
-          >
-            <Combobox.Empty>Fant ingen treff</Combobox.Empty>
-            {relatedConceptType === 'internal' &&
-              internalConcepts?.hits.map((concept) => (
-                <Combobox.Option
-                  key={concept.originaltBegrep as string}
-                  value={concept.originaltBegrep as string}
-                >
-                  {getTranslateText(concept.anbefaltTerm?.navn) as string}
-                </Combobox.Option>
-              ))}
-            {relatedConceptType === 'external' &&
-              externalConcepts?.hits.map((concept) => (
-                <Combobox.Option
-                  key={concept.id as string}
-                  value={concept.uri as string}
-                  description={getTranslateText(concept.organization?.prefLabel) as string}
-                >
-                  {getTranslateText(concept.title) as string}
-                </Combobox.Option>
-              ))}
-          </Combobox>
-        )}
-        {relatedConceptType === 'custom' && (
-          <Textfield
-            value={relatedConcept[0]}
-            onChange={handleCustomRelatedConceptChange}
-          />
-        )}
-      </Fieldset>
     </Box>
   );
 };
