017d711a53dcf69249da70f241dbb6ac693cb96b
Jeff Reiffers <jeffreiffers@gmail.com>
2024-12-05 12:51:39 +0100

    feat: display user role

--- apps/catalog-portal/app/layout.tsx
+++ apps/catalog-portal/app/layout.tsx
@@ -16,7 +16,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
             catalogAdminUrl={process.env.CATALOG_ADMIN_BASE_URI}
             fdkRegistrationBaseUrl={`${process.env.CATALOG_PORTAL_BASE_URI}/catalogs`}
             adminGuiBaseUrl={process.env.ADMIN_GUI_BASE_URI}
-            fdkCommunityBaseUrl={process.env.FDK_COMMUNITY_BASE_URI}
             fdkBaseUrl={process.env.FDK_BASE_URI}
             catalogTitle={localization.catalogOverview}
           >
