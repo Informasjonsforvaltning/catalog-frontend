ca5a055409ad2a425fb60ac6cefbe1423949778a
Jeff Reiffers <jeffreiffers@gmail.com>
2024-12-13 11:15:26 +0100

    fix: new concept

--- apps/concept-catalog/app/catalogs/[catalogId]/concepts/new/page.tsx
+++ apps/concept-catalog/app/catalogs/[catalogId]/concepts/new/page.tsx
@@ -1,11 +1,61 @@
-import { RedirectType, redirect } from 'next/navigation';
+import { getAllCodeLists, getConceptStatuses, getFields, getOrganization, getUsers } from '@catalog-frontend/data-access';
+import { BreadcrumbType } from '@catalog-frontend/ui';
+import { localization, prepareStatusList } from '@catalog-frontend/utils';
+import { CodeListsResult, FieldsResult, Organization, UsersResult } from '@catalog-frontend/types';
 import { withWriteProtectedPage } from '../../../../../utils/auth';
 
-export const NewPage = withWriteProtectedPage(
-  ({ catalogId }) => `/catalogs/${catalogId}/concepts/new`,
-  async ({ catalogId }) => {
-    //redirect(`${process.env.CONCEPT_CATALOG_GUI_BASE_URI}/${catalogId}/new`, RedirectType.push);
+import { NewPage } from './new-page.client';
+
+export default withWriteProtectedPage(
+  ({ catalogId }) => `/catalogs//${catalogId}/concepts/new`,
+  async ({ catalogId, session }) => {
+    const concept = {
+      ansvarligVirksomhet: {
+        id: catalogId
+      }
+    };
+    
+    const conceptStatuses = await getConceptStatuses()
+      .then((response) => response.json())
+      .then((body) => body?.conceptStatuses ?? [])
+      .then((statuses) => prepareStatusList(statuses));
+      
+    const organization: Organization = await getOrganization(catalogId).then((response) => response.json());
+    const codeListsResult: CodeListsResult = await getAllCodeLists(catalogId, `${session?.accessToken}`).then(
+      (response) => response.json(),
+    );
+    const fieldsResult: FieldsResult = await getFields(catalogId, `${session?.accessToken}`).then((response) =>
+      response.json(),
+    );
+    const usersResult: UsersResult = await getUsers(catalogId, `${session?.accessToken}`).then((response) =>
+      response.json(),
+    );
+    
+    const breadcrumbList = catalogId
+      ? ([
+          {
+            href: `/catalogs/${catalogId}`,
+            text: localization.catalogType.concept,
+          },
+          {
+            href: `/catalogs/${catalogId}/concepts/new`,
+            text: localization.newConcept,
+          },
+        ] as BreadcrumbType[])
+      : [];
+
+    return (
+      <NewPage
+        catalogId={catalogId}
+        breadcrumbList={breadcrumbList}
+        organization={organization}
+        concept={concept}
+        conceptStatuses={conceptStatuses}
+        codeListsResult={codeListsResult}
+        fieldsResult={fieldsResult}
+        usersResult={usersResult}
+        catalogPortalBaseUri={process.env.CATALOG_PORTAL_BASE_URI}
+      />
+    );
   },
 );
-
-export default NewPage;
