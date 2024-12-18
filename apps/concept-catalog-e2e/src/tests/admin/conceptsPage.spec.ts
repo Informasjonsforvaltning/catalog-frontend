7d3ec12f5348b6c02efe4b053d18001d5dfb64b2
Jeff Reiffers <jeffreiffers@gmail.com>
2024-12-18 08:55:08 +0100

    feat: concept-catalog e2e

--- apps/concept-catalog-e2e/src/tests/admin/conceptsPage.spec.ts
+++ apps/concept-catalog-e2e/src/tests/admin/conceptsPage.spec.ts
@@ -1,13 +1,17 @@
 import { ALL_CONCEPTS, CONCEPT_1, CONCEPT_2, CONCEPT_3 } from '../../data/concepts';
 import { runTestAsAdmin } from '../../fixtures/basePage';
+import ConceptsPage from '../../page-object-model/conceptsPage';
 
-runTestAsAdmin('test if the search page renders correctly', async ({ conceptsPage }) => {
+type TestProps = { conceptsPage: ConceptsPage };
+
+runTestAsAdmin('test if the search page renders correctly', async ({ conceptsPage }: TestProps) => {
   await conceptsPage.goto();
+  //TODO await conceptsPage.checkAccessibility();
   await conceptsPage.expectFiltersToBeVisible();
   await conceptsPage.expectSearchResults(ALL_CONCEPTS);
 });
 
-runTestAsAdmin('test search', async ({ conceptsPage }) => {
+runTestAsAdmin('test search', async ({ conceptsPage }: TestProps) => {
   await conceptsPage.goto();
   await conceptsPage.search('test concept 2');
   await conceptsPage.expectSearchResults([CONCEPT_2], [CONCEPT_1, CONCEPT_3]);
@@ -16,34 +20,19 @@ runTestAsAdmin('test search', async ({ conceptsPage }) => {
   await conceptsPage.expectSearchResults(ALL_CONCEPTS);
 });
 
-runTestAsAdmin('test status filter', async ({ conceptsPage }) => {
+runTestAsAdmin('test status filter', async ({ conceptsPage }: TestProps) => {
   await conceptsPage.goto();
-  await conceptsPage.filterStatusCompleted();
+  await conceptsPage.filterStatusDraft();
   await conceptsPage.expectSearchResults([CONCEPT_1], [CONCEPT_2, CONCEPT_3]);
 
   await conceptsPage.clearFilters();
-  await conceptsPage.filterStatusDeprecated();
+  await conceptsPage.filterStatusCurrent();
   await conceptsPage.expectSearchResults([CONCEPT_2], [CONCEPT_1, CONCEPT_3]);
 
   await conceptsPage.clearFilters();
-  await conceptsPage.filterStatusWithDrawn();
+  await conceptsPage.filterStatusRejected();
   await conceptsPage.expectSearchResults([CONCEPT_3], [CONCEPT_1, CONCEPT_2]);
 
   await conceptsPage.clearFilters();
-  await conceptsPage.filterStatusUnderDevelopment();
-  await conceptsPage.expectSearchResults([]);
-
-  await conceptsPage.clearFilters();
-  await conceptsPage.expectSearchResults(ALL_CONCEPTS);
-});
-
-runTestAsAdmin('test published state filter', async ({ conceptsPage }) => {
-  await conceptsPage.goto();
-  await conceptsPage.filterPublished();
-  await conceptsPage.expectSearchResults([]);
-
-  await conceptsPage.clearFilters();
-  await conceptsPage.expectSearchResults(ALL_CONCEPTS);
-  await conceptsPage.filterNotPublished();
   await conceptsPage.expectSearchResults(ALL_CONCEPTS);
 });
