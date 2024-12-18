772eec0478b6441a9477b411a906870e29a21ed6
Jeff Reiffers <jeffreiffers@gmail.com>
2024-12-18 13:16:06 +0100

    chore: use search component from designsystemet and set all components to small

--- libs/utils/src/lib/language/nb.ts
+++ libs/utils/src/lib/language/nb.ts
@@ -144,6 +144,7 @@ export const nb = {
     searchForUsername: 'Søk etter brukernavn...',
     searchInCatalogs: ['Søk i Felles datakatalog', 'Lenke til søk i felles datakatalog'],
     sort: 'Sortering',
+    clear: 'Tøm', 
 
     fields: {
       alleFelter: 'Alle felter',
