bbac0d6cf58f73deaea1cc33aad91bd6c6b285c2
hegeaal <hegeaal@gmail.com>
2025-01-22 13:20:18 +0100

    fix: redirect should be async + delete default test dataset-catalog

--- apps/catalog-portal/app/page.tsx
+++ apps/catalog-portal/app/page.tsx
@@ -1,7 +1,8 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-const Home = () => {
+const Home = async () => {
+  // NOTICE: Call cookies() to opt into dynamic rendering
   cookies();
   redirect(`${process.env.CATALOG_PORTAL_BASE_URI}/catalogs`);
 };
