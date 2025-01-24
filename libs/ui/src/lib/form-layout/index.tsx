740ee74cd55d58090e74c2049374bede6efd6a60
Jeff Reiffers <jeffreiffers@gmail.com>
2025-01-24 10:49:04 +0100

    fix: invalid menu ref for contact

--- libs/ui/src/lib/form-layout/index.tsx
+++ libs/ui/src/lib/form-layout/index.tsx
@@ -2,7 +2,7 @@
 
 import React, { Children, PropsWithChildren, ReactNode, useRef, useState } from 'react';
 import styles from './form-layout.module.scss';
-import { Alert, Checkbox, Heading, Link, Paragraph } from '@digdir/designsystemet-react';
+import { Alert, Heading, Link, Paragraph } from '@digdir/designsystemet-react';
 import { useIntersectionObserver } from '../intersection-observer';
 import classNames from 'classnames';
 
@@ -45,19 +45,23 @@ const SideMenu = ({ heading, children }: SideMenuProps) => {
   );
 };
 
-const MenuItem = ({ active, required = false, children }: PropsWithChildren & { active: boolean; required?: boolean }) => {
+const MenuItem = ({
+  active,
+  required = false,
+  children,
+}: PropsWithChildren & { active: boolean; required?: boolean }) => {
   return <li className={classNames(active ? styles.active : {}, required ? styles.required : {})}>{children}</li>;
 };
 
 export const FormLayout = ({ children }: FormLayoutProps) => {
   const childrenArray = Children.toArray(children);
+  const sectionArray = childrenArray
+    .filter((child) => React.isValidElement(child) && child.type === Section)
+    .map((child) => child as React.ReactElement);
 
   const [activeSection, setActiveSection] = useState('');
-  const sectionRefs = useRef(
-    childrenArray
-      .filter((child) => React.isValidElement(child) && child.type === Section)
-      .map(() => React.createRef<HTMLDivElement>()),
-  );
+  const sectionRefs = useRef(sectionArray.map(() => React.createRef<HTMLDivElement>()));
+
   const [observerEnabled, setObserverEnabled] = useState(true);
   const options = childrenArray.filter((child) => React.isValidElement(child) && child.type === Options)[0];
 
@@ -87,44 +91,39 @@ export const FormLayout = ({ children }: FormLayoutProps) => {
       <div className={styles.grid}>
         <div
           className={styles.sideMenu}
-          style={{ height: `${((childrenArray.length) * 2.4479) + 2.95}rem` }}
+          style={{ height: `${childrenArray.length * 2.4479 + 2.95}rem` }}
         >
           <SideMenu heading='Innhold i skjema'>
-            {childrenArray.map((child, index) => {
-              if (React.isValidElement(child) && child.type === Section) {
-                const { id, title, required } = child.props as SectionProps;
-                return (
-                  <MenuItem
-                    key={`menu-item-${id}`}
-                    active={activeSection === id}
-                    required={required}
-                  >
-                    <Link onClick={() => handleNavClick(id, index)}>{title} {required && (<span className={styles.required}>(Må fylles ut)</span>)}</Link>
-                  </MenuItem>
-                );
-              } else {
-                return null;
-              }
+            {sectionArray.map((child, index) => {
+              const { id, title, required } = child.props as SectionProps;
+              return (
+                <MenuItem
+                  key={`menu-item-${id}`}
+                  active={activeSection === id}
+                  required={required}
+                >
+                  <Link onClick={() => handleNavClick(id, index)}>
+                    {title} {required && <span className={styles.required}>(Må fylles ut)</span>}
+                  </Link>
+                </MenuItem>
+              );
             })}
           </SideMenu>
         </div>
         <div className={styles.content}>
-          {options && (
-            <div>{options}</div>
-          )}
-          {React.Children.map(children, (child, index) => {
-            if (React.isValidElement(child) && child.type === Section) {
-              const { id } = child.props as SectionProps;
-              return (
-                <div
-                  id={id}
-                  key={id}
-                  ref={sectionRefs.current[index]}
-                >
-                  {child}
-                </div>
-              );
-            }
+          {options && <div>{options}</div>}
+          {sectionArray.map((section, index) => {
+            const { id } = section.props as SectionProps;
+            const ref = sectionRefs.current[index];
+            return (
+              <div
+                id={id}
+                key={id}
+                ref={ref}
+              >
+                {section}
+              </div>
+            );
           })}
         </div>
       </div>
@@ -147,11 +146,7 @@ const Section = ({ id, title, subtitle, children }: SectionProps) => (
   </div>
 );
 
-const Options = ({ children }: OptionsProps) => (
-<Alert>
-  {children}
-</Alert>
-);
+const Options = ({ children }: OptionsProps) => <Alert>{children}</Alert>;
 
 FormLayout.Section = Section;
 FormLayout.Options = Options;
