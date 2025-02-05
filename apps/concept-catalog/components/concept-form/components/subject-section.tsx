c52e495b0ac5ae59bbb9e9fe8c3ac1ac2284f8c2
Jeff Reiffers <jeffreiffers@gmail.com>
2025-02-05 09:44:07 +0100

    fix: show disabled chips for deaktivated subject values

--- apps/concept-catalog/components/concept-form/components/subject-section.tsx
+++ apps/concept-catalog/components/concept-form/components/subject-section.tsx
@@ -1,11 +1,11 @@
 import { useFormikContext } from 'formik';
-import { Alert, Box, Button, Combobox, Heading, Paragraph } from '@digdir/designsystemet-react';
+import { Alert, Button, Combobox, Heading, Paragraph } from '@digdir/designsystemet-react';
 import { Code, Concept } from '@catalog-frontend/types';
-import { FormikLanguageFieldset, TitleWithHelpTextAndTag } from '@catalog-frontend/ui';
+import { FormikLanguageFieldset, FormikMultivalueTextfield, TitleWithHelpTextAndTag } from '@catalog-frontend/ui';
 import { localization } from '@catalog-frontend/utils';
 import styles from '../concept-form.module.scss';
 import { getParentPath } from '../../../utils/codeList';
-import { ReactNode, useEffect } from 'react';
+import { ReactNode } from 'react';
 import { isEmpty } from 'lodash';
 
 type SubjectSectionProps = {
@@ -80,6 +80,7 @@ export const SubjectSection = ({ codes }: SubjectSectionProps) => {
     if (!codeListActivated || !isEmpty(values.fagområde)) {
       fields.push(
         <FormikLanguageFieldset
+          key='fagområde'
           name='fagområde'
           errorMessage={localization.conceptForm.validation.languageRequired}
           errorArgs={{ label: localization.conceptForm.fieldLabel.subjectFree }}
@@ -87,10 +88,12 @@ export const SubjectSection = ({ codes }: SubjectSectionProps) => {
           readOnly={codeListActivated}
           legend={
             <TitleWithHelpTextAndTag
-              {...!codeListActivated ? {
-              tagTitle: localization.tag.recommended,
-              tagColor: 'info'
-            } : {}}
+              {...(!codeListActivated
+                ? {
+                    tagTitle: localization.tag.recommended,
+                    tagColor: 'info',
+                  }
+                : {})}
               helpText={localization.conceptForm.helpText.subjectFree}
             >
               {localization.conceptForm.fieldLabel.subjectFree}
@@ -100,24 +103,29 @@ export const SubjectSection = ({ codes }: SubjectSectionProps) => {
       );
     }
 
-    if (codeListActivated || !isEmpty(values.fagområdeKoder)) {
-      fields.push(
-        <Combobox
-          multiple
-          size='sm'
-          hideClearButton
-          readOnly={!codeListActivated}
-          label={
-            <TitleWithHelpTextAndTag
-              {...codeListActivated ? {
-                tagTitle: localization.tag.recommended,
-                tagColor: 'info'
-              } : {}}              
+    const codeListLabel = (
+      <TitleWithHelpTextAndTag
+              {...(codeListActivated
+                ? {
+                    tagTitle: localization.tag.recommended,
+                    tagColor: 'info',
+                  }
+                : {})}
               helpText={localization.conceptForm.helpText.subjectCodeList}
             >
               {localization.conceptForm.fieldLabel.subjectCodeList}
             </TitleWithHelpTextAndTag>
-          }
+    );
+
+    if (codeListActivated) {
+      fields.push(
+        <Combobox
+          key='fagområdeKoder'
+          multiple
+          size='sm'
+          hideClearButton
+          readOnly={!codeListActivated}
+          label={codeListLabel}
           value={selected ?? []}
           onValueChange={(value) => setFieldValue('fagområdeKoder', value)}
           error={errors.fagområdeKoder}
@@ -136,6 +144,15 @@ export const SubjectSection = ({ codes }: SubjectSectionProps) => {
             );
           })}
         </Combobox>,
+      );
+    } else if (!isEmpty(values.fagområdeKoder)) {
+      fields.push(
+        <FormikMultivalueTextfield
+          key='fagområdeKoder'
+          name='fagområdeKoder'
+          label={codeListLabel}
+          readOnly
+        />,
       );
     }
 
