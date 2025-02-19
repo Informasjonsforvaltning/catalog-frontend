836c5c97a4969c2ed3669817e34a17bfd3beced5
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-02-19 16:34:47 +0100

    feat: title, status and buttons in API details page

--- apps/data-service-catalog/app/catalogs/[catalogId]/data-services/[dataServiceId]/page.tsx
+++ apps/data-service-catalog/app/catalogs/[catalogId]/data-services/[dataServiceId]/page.tsx
@@ -2,8 +2,16 @@ import { Breadcrumbs, BreadcrumbType, PageBanner } from '@catalog-frontend/ui';
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 import { getTranslateText, getValidSession, localization } from '@catalog-frontend/utils';
 import { Organization } from '@catalog-frontend/types';
-import { getDataServiceById, getOrganization } from '@catalog-frontend/data-access';
+import {
+  getCurrencies,
+  getDataServiceById,
+  getDistributionStatuses,
+  getOpenLicenses,
+  getOrganization,
+  getPlannedAvailabilities,
+} from '@catalog-frontend/data-access';
 import { redirect, RedirectType } from 'next/navigation';
+import DataServiceDetailsPageClient from './data-service-details-page-client';
 
 export default async function EditDataServicePage({ params }: Params) {
   const { catalogId, dataServiceId } = params;
@@ -16,6 +24,20 @@ export default async function EditDataServicePage({ params }: Params) {
   }
 
   const organization: Organization = await getOrganization(catalogId).then((res) => res.json());
+
+  const [licenseResponse, statusResponse, availabilitiesResponse, currenciesResponse] = await Promise.all([
+    getOpenLicenses().then((res) => res.json()),
+    getDistributionStatuses().then((res) => res.json()),
+    getPlannedAvailabilities().then((res) => res.json()),
+    getCurrencies().then((res) => res.json()),
+  ]);
+
+  const referenceData = {
+    distributionStatuses: statusResponse.distributionStatuses,
+    openLicenses: licenseResponse.openLicenses,
+    plannedAvailabilities: availabilitiesResponse.plannedAvailabilities,
+    currencies: currenciesResponse.currencies,
+  };
 
   const breadcrumbList = [
     {
@@ -38,7 +60,14 @@ export default async function EditDataServicePage({ params }: Params) {
         title={localization.catalogType.dataService}
         subtitle={getTranslateText(organization.prefLabel).toString()}
       />
-      <div className='container'>Her kommer detaljside for {getTranslateText(dataService.title)}</div>
+      <div className='container'>
+        <DataServiceDetailsPageClient
+          dataService={dataService}
+          catalogId={catalogId}
+          dataServiceId={dataServiceId}
+          referenceData={referenceData}
+        ></DataServiceDetailsPageClient>
+      </div>
     </>
   );
 }
