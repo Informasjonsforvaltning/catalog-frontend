86c4502e2ee1bd19556357f995df729f0220f8bc
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-02-21 10:38:47 +0100

    feat: add endpoint & documentation links to API details page

---
+++ apps/data-service-catalog/components/details-page-columns/components/details-url-list.tsx
@@ -0,0 +1,22 @@
+import { List } from '@digdir/designsystemet-react';
+
+type Props = {
+  urls: string[] | undefined;
+};
+
+export const DetailsUrlList = ({ urls }: Props) => {
+  return (
+    <List.Root size={'sm'}>
+      <List.Unordered
+        style={{
+          listStyle: 'none',
+          paddingLeft: 0,
+        }}
+      >
+        {urls?.map((item, index) => {
+          return item ? <List.Item key={`urls-${index}`}>{item}</List.Item> : null;
+        })}
+      </List.Unordered>
+    </List.Root>
+  );
+};
