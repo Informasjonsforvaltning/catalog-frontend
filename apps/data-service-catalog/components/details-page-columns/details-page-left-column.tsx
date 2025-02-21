5261d5cabeab599cd545d6fdfc5743496744beb5
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-02-21 10:38:47 +0100

    feat: add endpoint & documentation links to API details page

--- apps/data-service-catalog/components/details-page-columns/details-page-left-column.tsx
+++ apps/data-service-catalog/components/details-page-columns/details-page-left-column.tsx
@@ -3,7 +3,8 @@ import styles from './details-columns.module.css';
 import { InfoCard } from '@catalog-frontend/ui';
 import { isEmpty } from 'lodash';
 import { getTranslateText, localization } from '@catalog-frontend/utils';
-import { Paragraph, Tag } from '@digdir/designsystemet-react';
+import { List, Paragraph, Tag } from '@digdir/designsystemet-react';
+import { DetailsUrlList } from './components/details-url-list';
 
 type Props = {
   dataService: DataService;
@@ -15,9 +16,10 @@ export const LeftColumn = ({ dataService, language }: Props) => {
     <InfoCard>
       {!isEmpty(dataService?.description) && (
         <InfoCard.Item title={localization.dataServiceForm.fieldLabel.description}>
-          <Paragraph size='sm'>{getTranslateText(dataService?.description, language)} </Paragraph>
+          <Paragraph size='sm'>{getTranslateText(dataService?.description, language)}</Paragraph>
         </InfoCard.Item>
       )}
+
       {!isEmpty(dataService?.keywords) && (
         <InfoCard.Item title={localization.dataServiceForm.fieldLabel.keywords}>
           <li className={styles.list}>
@@ -33,6 +35,30 @@ export const LeftColumn = ({ dataService, language }: Props) => {
               ) : null;
             })}
           </li>
+        </InfoCard.Item>
+      )}
+
+      {!isEmpty(dataService?.endpointUrl) && (
+        <InfoCard.Item title={localization.dataServiceForm.fieldLabel.endpoint}>
+          {dataService?.endpointUrl}
+        </InfoCard.Item>
+      )}
+
+      {!isEmpty(dataService?.endpointDescriptions) && (
+        <InfoCard.Item title={localization.dataServiceForm.fieldLabel.endpointDescriptions}>
+          <DetailsUrlList urls={dataService.endpointDescriptions} />
+        </InfoCard.Item>
+      )}
+
+      {!isEmpty(dataService?.landingPage) && (
+        <InfoCard.Item title={localization.dataServiceForm.fieldLabel.landingPage}>
+          {dataService?.landingPage}
+        </InfoCard.Item>
+      )}
+
+      {!isEmpty(dataService?.pages) && (
+        <InfoCard.Item title={localization.dataServiceForm.fieldLabel.pages}>
+          <DetailsUrlList urls={dataService.pages} />
         </InfoCard.Item>
       )}
     </InfoCard>
