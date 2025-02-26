a503b4f5aef5ae0104ba7ce2b3eb6598b0b5d896
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-02-26 11:15:12 +0100

    feat: add costs to API details page

---
+++ apps/data-service-catalog/components/details-page-columns/components/cost-list.tsx
@@ -0,0 +1,52 @@
+import { DataServiceCost } from '@catalog-frontend/types';
+import { Card, Divider, Link, List, Paragraph } from '@digdir/designsystemet-react';
+import React from 'react';
+import { getTranslateText } from '@catalog-frontend/utils';
+import styles from '../../data-service-form/data-service-form.module.css';
+
+type Props = {
+  costs: DataServiceCost[] | undefined;
+  language: string;
+};
+
+export const CostList = ({ costs, language }: Props) => {
+  return (
+    <>
+      {costs?.map((cost, i) => (
+        <Card
+          key={`costs-card-${i}`}
+          color='neutral'
+        >
+          <Card.Content>
+            <List.Root>
+              <List.Unordered
+                style={{
+                  listStyle: 'none',
+                  paddingLeft: 0,
+                }}
+              >
+                {cost.value && (
+                  <List.Item>
+                    {cost.value} {cost.currency?.split('/')?.reverse()[0] ?? ''}
+                  </List.Item>
+                )}
+
+                {cost.documentation?.map((doc, docIndex) => (
+                  <List.Item key={`costs-${i}-doc-${docIndex}`}>
+                    <Link
+                      href={doc}
+                      target='_blank'
+                    >
+                      {doc}
+                    </Link>
+                  </List.Item>
+                ))}
+              </List.Unordered>
+            </List.Root>
+            <Paragraph size={'sm'}>{getTranslateText(cost.description, language)}</Paragraph>
+          </Card.Content>
+        </Card>
+      ))}
+    </>
+  );
+};
