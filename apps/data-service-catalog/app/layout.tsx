4138d9eb4f25fd7941c2e6ee3c7b1f0b2bba6f22
hegeaal <hegeaal@gmail.com>
2025-02-26 21:18:39 +0100

    feat: add designbanner to dataservice-catalog

--- apps/data-service-catalog/app/layout.tsx
+++ apps/data-service-catalog/app/layout.tsx
@@ -1,6 +1,7 @@
-import { Layout, NextAuthProvider, ReactQueryClientProvider } from '@catalog-frontend/ui';
+import { NextAuthProvider, ReactQueryClientProvider } from '@catalog-frontend/ui';
 import { localization } from '@catalog-frontend/utils';
 import { Metadata } from 'next';
+import { CatalogLayout } from '../components/catalog-layout';
 
 export const metadata: Metadata = {
   title: localization.catalogType.dataService,
@@ -13,16 +14,16 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       <body>
         <NextAuthProvider>
           <ReactQueryClientProvider>
-            <Layout
+            <CatalogLayout
               catalogAdminUrl={process.env.CATALOG_ADMIN_BASE_URI}
+              catalogAdminServiceUrl={process.env.CATALOG_ADMIN_SERVICE_BASE_URI}
               fdkRegistrationBaseUrl={`${process.env.CATALOG_PORTAL_BASE_URI}/catalogs`}
               adminGuiBaseUrl={process.env.ADMIN_GUI_BASE_URI}
               fdkCommunityBaseUrl={process.env.FDK_COMMUNITY_BASE_URI}
               fdkBaseUrl={process.env.FDK_BASE_URI}
-              catalogTitle={localization.catalogType.dataService}
             >
               {children}
-            </Layout>
+            </CatalogLayout>
           </ReactQueryClientProvider>
         </NextAuthProvider>
       </body>
