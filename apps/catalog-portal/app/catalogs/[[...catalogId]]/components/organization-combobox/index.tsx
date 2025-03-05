de14f6574f0f5eb670027dac1da2739cc8afdbac
Jeff Reiffers <jeffreiffers@gmail.com>
2025-03-05 14:11:24 +0100

    fix: support system admin

--- apps/catalog-portal/app/catalogs/[[...catalogId]]/components/organization-combobox/index.tsx
+++ apps/catalog-portal/app/catalogs/[[...catalogId]]/components/organization-combobox/index.tsx
@@ -3,31 +3,36 @@
 import { getTranslateText, sortAscending } from '@catalog-frontend/utils';
 
 import { Organization } from '@catalog-frontend/types';
-import { Combobox } from '@digdir/designsystemet-react';
+import { Combobox, Spinner } from '@digdir/designsystemet-react';
 import { useRouter } from 'next/navigation';
+import styles from './organization-combobox.module.css';
+import { useState } from 'react';
 
 type OrganizationComboboxProps = {
   organizations: Organization[];
 };
 
 const OrganizationCombobox = (props: OrganizationComboboxProps) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  
   const { organizations } = props;
 
   const sortedOrganization = organizations.sort((a, b) =>
     sortAscending(getTranslateText(a.prefLabel).toString(), getTranslateText(b.prefLabel).toString()),
   );
 
-  const router = useRouter();
-
   return (
-    <div style={{ width: 500 }}>
+    <div className={styles.container}>
       <Combobox
+        className={styles.combobox}
         size='sm'
         label={'Virksomhet'}
         placeholder='Velg en virksomhet'
         onValueChange={(value) => {
           const match = organizations.find((org) => org.organizationId === value[0]);
           if (match) {
+            setLoading(true);
             router.push(`/catalogs/${value[0]}`);
           }
         }}
@@ -41,6 +46,9 @@ const OrganizationCombobox = (props: OrganizationComboboxProps) => {
           </Combobox.Option>
         ))}
       </Combobox>
+      {loading && (
+        <Spinner title={'Laster virksomhet'} />
+      )}      
     </div>
   );
 };
