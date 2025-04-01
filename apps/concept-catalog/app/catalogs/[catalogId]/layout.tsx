3b69c58d17b684b2729a306edbe05d0e4112993c
Jeff Reiffers <jeffreiffers@gmail.com>
2025-04-01 10:07:07 +0200

    chore: disable caching

--- apps/concept-catalog/app/catalogs/[catalogId]/layout.tsx
+++ apps/concept-catalog/app/catalogs/[catalogId]/layout.tsx
@@ -1,6 +1,8 @@
 import { TermsOfUseModal } from '@catalog-frontend/ui';
 import { CatalogLayout } from '../../../components/catalog-layout';
 
+export const fetchCache = 'force-no-store';
+
 const Layout = ({ children, params: { catalogId } }: { children: React.ReactNode; params: { catalogId: string } }) => {
   return (
     <CatalogLayout
