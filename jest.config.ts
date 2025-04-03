00dd9db8e324a7f8fb8c5e72d42d9436fb50d635
hegeaal <hegeaal@gmail.com>
2025-04-03 08:57:26 +0200

    fix: update jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,16 @@
-import { getJestProjects } from '@nrwl/jest';
+import { workspaceRoot } from '@nx/devkit';
+import { readdirSync } from 'fs';
+import { join } from 'path';
+
+function getProjects() {
+  const appsDir = join(workspaceRoot, 'apps');
+  const libsDir = join(workspaceRoot, 'libs');
+  return [
+    ...readdirSync(appsDir).map((dir) => `<rootDir>/apps/${dir}`),
+    ...readdirSync(libsDir).map((dir) => `<rootDir>/libs/${dir}`),
+  ];
+}
 
 export default {
-  projects: getJestProjects(),
+  projects: getProjects(),
 };
---
+++ libs/utils/src/lib/tests/setupMocks.js
@@ -0,0 +1,27 @@
+// Mocks next/router
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({ query: {} }),
+}));
+
+// Mocks react-localization
+jest.mock('react-localization', () => {
+  return jest.fn().mockImplementation((translations) => {
+    let currentLanguage = 'nb';
+
+    return {
+      setLanguage: jest.fn((language) => {
+        currentLanguage = language;
+      }),
+      getLanguage: jest.fn(() => currentLanguage),
+      getString: jest.fn((key) => {
+        const keys = key.split('.');
+        let value = translations[currentLanguage];
+        for (const k of keys) {
+          value = value ? value[k] : null;
+        }
+        return value || key;
+      }),
+      ...translations[currentLanguage],
+    };
+  });
+});
