ce66c6f170a3742be5abe7f21a0fb5fa60cf9302
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-05-14 12:57:23 +0200

    refactor: use FieldArray in UriWithLabel component

--- apps/dataset-catalog/components/dataset-form/components/uri-with-label-field-set-table.tsx
+++ apps/dataset-catalog/components/dataset-form/components/uri-with-label-field-set-table.tsx
@@ -9,7 +9,7 @@ import {
 } from '@catalog-frontend/ui';
 import { getTranslateText, localization, trimObjectWhitespace } from '@catalog-frontend/utils';
 import { Button, Modal, Table, Textfield } from '@digdir/designsystemet-react';
-import { FastField, Formik, useFormikContext } from 'formik';
+import { FastField, FieldArray, Formik } from 'formik';
 import styles from '../dataset-form.module.css';
 import { ReactNode, useRef, useState } from 'react';
 import { trim, isEmpty, pickBy, identity } from 'lodash';
@@ -34,62 +34,55 @@ const hasNoFieldValues = (values: UriWithLabel) => {
 };
 
 export const UriWithLabelFieldsetTable = ({ fieldName, values, label }: Props) => {
-  const { setFieldValue } = useFormikContext();
-
-  function removeAtIndex(arr: UriWithLabel[], index: number) {
-    arr.splice(index, 1);
-    return arr;
-  }
-
   return (
     <div className={styles.fieldContainer}>
       {typeof label === 'string' ? <FormHeading>{label}</FormHeading> : label}
-      {values && values?.length > 0 && (
-        <Table
-          size='sm'
-          className={styles.table}
-        >
-          <Table.Head>
-            <Table.Row>
-              <Table.HeaderCell>{localization.title}</Table.HeaderCell>
-              <Table.HeaderCell>{localization.link}</Table.HeaderCell>
-              <Table.HeaderCell aria-label='Actions' />
-            </Table.Row>
-          </Table.Head>
-          <Table.Body>
-            {values?.map((item, index) => (
-              <Table.Row key={`${fieldName}-tableRow-${index}`}>
-                <Table.Cell>{getTranslateText(item?.prefLabel)}</Table.Cell>
-                <Table.Cell>{item?.uri}</Table.Cell>
-                <Table.Cell>
-                  <span className={styles.set}>
-                    <FieldModal
-                      fieldName={fieldName}
-                      template={item}
-                      type={'edit'}
-                      onSuccess={(updatedItem: UriWithLabel) => setFieldValue(`${fieldName}[${index}]`, updatedItem)}
-                    />
-                    <DeleteButton onClick={() => setFieldValue(fieldName, removeAtIndex(values, index))} />
-                  </span>
-                </Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table>
-      )}
-      <div>
-        <FieldModal
-          fieldName={fieldName}
-          template={{ prefLabel: { nb: '' }, uri: '' }}
-          type={'new'}
-          onSuccess={(formValues) =>
-            setFieldValue(
-              values && values?.length > 0 ? `${fieldName}[${values?.length}]` : `${fieldName}[0]`,
-              formValues,
-            )
-          }
-        />
-      </div>
+      <FieldArray
+        name={fieldName}
+        render={(arrayHelpers) => (
+          <>
+            <Table
+              size='sm'
+              className={styles.table}
+            >
+              <Table.Head>
+                <Table.Row>
+                  <Table.HeaderCell>{localization.title}</Table.HeaderCell>
+                  <Table.HeaderCell>{localization.link}</Table.HeaderCell>
+                  <Table.HeaderCell aria-label='Actions' />
+                </Table.Row>
+              </Table.Head>
+              <Table.Body>
+                {values?.map((item, index) => (
+                  <Table.Row key={`${fieldName}-tableRow-${index}`}>
+                    <Table.Cell>{getTranslateText(item?.prefLabel)}</Table.Cell>
+                    <Table.Cell>{item?.uri}</Table.Cell>
+                    <Table.Cell>
+                      <span className={styles.set}>
+                        <FieldModal
+                          fieldName={fieldName}
+                          template={item}
+                          type={'edit'}
+                          onSuccess={(updatedItem: UriWithLabel) => arrayHelpers.replace(index, updatedItem)}
+                        />
+                        <DeleteButton onClick={() => arrayHelpers.remove(index)} />
+                      </span>
+                    </Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table>
+            <div>
+              <FieldModal
+                fieldName={fieldName}
+                template={{ prefLabel: { nb: '' }, uri: '' }}
+                type={'new'}
+                onSuccess={(formValues) => arrayHelpers.push(formValues)}
+              />
+            </div>
+          </>
+        )}
+      />
     </div>
   );
 };
