d161c8aea897d23c5abeb0f828f935bc12d85ee3
Nils Ove Tendenes <nils.ove@tendenes.com>
2025-05-29 10:57:43 +0200

    refactor: update data-service pages

--- apps/data-service-catalog/app/catalogs/[catalogId]/data-services/new/page.tsx
+++ apps/data-service-catalog/app/catalogs/[catalogId]/data-services/new/page.tsx
@@ -9,54 +9,60 @@ import {
 import DataServiceForm from '../../../../../components/data-service-form';
 import { dataServiceToBeCreatedTemplate } from '../../../../../components/data-service-form/utils/data-service-initial-values';
 import { localization } from '@catalog-frontend/utils';
-export default async function NewDataServicePage({ params }: { params: Promise<{ catalogId: string }> }) {
-  const { catalogId } = await params;
-  const initialValues = dataServiceToBeCreatedTemplate();
-  const searchEnv = process.env.FDK_SEARCH_SERVICE_BASE_URI ?? '';
-  const referenceDataEnv = process.env.FDK_BASE_URI ?? '';
+import { withWriteProtectedPage } from '@data-service-catalog/utils/auth';
 
-  const [licenseResponse, statusResponse, availabilitiesResponse, currenciesResponse] = await Promise.all([
-    getOpenLicenses().then((res) => res.json()),
-    getDistributionStatuses().then((res) => res.json()),
-    getPlannedAvailabilities().then((res) => res.json()),
-    getCurrencies().then((res) => res.json()),
-  ]);
+const NewDataServicePage = withWriteProtectedPage(
+  ({ catalogId }) => `/catalogs/${catalogId}/data-services/new`,
+  async ({ catalogId }) => {
+    const initialValues = dataServiceToBeCreatedTemplate();
+    const searchEnv = process.env.FDK_SEARCH_SERVICE_BASE_URI ?? '';
+    const referenceDataEnv = process.env.FDK_BASE_URI ?? '';
 
-  const referenceData = {
-    openLicenses: licenseResponse.openLicenses,
-    distributionStatuses: statusResponse.distributionStatuses,
-    plannedAvailabilities: availabilitiesResponse.plannedAvailabilities,
-    currencies: currenciesResponse.currencies,
-  };
+    const [licenseResponse, statusResponse, availabilitiesResponse, currenciesResponse] = await Promise.all([
+      getOpenLicenses().then((res) => res.json()),
+      getDistributionStatuses().then((res) => res.json()),
+      getPlannedAvailabilities().then((res) => res.json()),
+      getCurrencies().then((res) => res.json()),
+    ]);
 
-  const breadcrumbList = [
-    {
-      href: `/catalogs/${catalogId}/data-services`,
-      text: localization.catalogType.dataService,
-    },
-    {
-      href: `/catalogs/${catalogId}/data-services/new`,
-      text: localization.dataServiceCatalog.button.newDataService,
-    },
-  ] as BreadcrumbType[];
+    const referenceData = {
+      openLicenses: licenseResponse.openLicenses,
+      distributionStatuses: statusResponse.distributionStatuses,
+      plannedAvailabilities: availabilitiesResponse.plannedAvailabilities,
+      currencies: currenciesResponse.currencies,
+    };
 
-  return (
-    <>
-      <Breadcrumbs
-        breadcrumbList={breadcrumbList}
-        catalogPortalUrl={`${process.env.CATALOG_PORTAL_BASE_URI}/catalogs`}
-      />
-      <DesignBanner
-        title={localization.catalogType.dataService}
-        catalogId={catalogId}
-      />
-      <DataServiceForm
-        initialValues={initialValues}
-        searchEnv={searchEnv}
-        referenceData={referenceData}
-        referenceDataEnv={referenceDataEnv}
-        submitType={'create'}
-      />
-    </>
-  );
-}
+    const breadcrumbList = [
+      {
+        href: `/catalogs/${catalogId}/data-services`,
+        text: localization.catalogType.dataService,
+      },
+      {
+        href: `/catalogs/${catalogId}/data-services/new`,
+        text: localization.dataServiceCatalog.button.newDataService,
+      },
+    ] as BreadcrumbType[];
+
+    return (
+      <>
+        <Breadcrumbs
+          breadcrumbList={breadcrumbList}
+          catalogPortalUrl={`${process.env.CATALOG_PORTAL_BASE_URI}/catalogs`}
+        />
+        <DesignBanner
+          title={localization.catalogType.dataService}
+          catalogId={catalogId}
+        />
+        <DataServiceForm
+          initialValues={initialValues}
+          searchEnv={searchEnv}
+          referenceData={referenceData}
+          referenceDataEnv={referenceDataEnv}
+          submitType={'create'}
+        />
+      </>
+    );
+  },
+);
+
+export default NewDataServicePage;
