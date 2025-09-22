7b9f68d04797659ae71707cc6f3520435ebba2c1
Ahmed Abdelgawwad <ahmed.abdulgawad91@gmail.com>
2025-09-22 09:22:49 +0200

    fix: View progress when the imported RDF concepts are in In_Progress/Pågår stage

---
+++ apps/concept-catalog/app/api/catalogs/[catalogId]/concepts/import-results/[resultId]/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest } from 'next/server';
+import { withValidSessionForApi } from '@catalog-frontend/utils';
+import { getConceptImportResultById, removeImportResultConcept } from '@catalog-frontend/data-access';
+
+export const DELETE = async (req: NextRequest,
+                             props: { params: Promise<{ catalogId: string, resultId: string }> }) => {
+  const params = await props.params;
+  return await withValidSessionForApi(async (session) => {
+    const { catalogId, resultId } = params;
+
+    try {
+      const response = await removeImportResultConcept(catalogId, resultId, session?.accessToken);
+      if (response.status !== 200) {
+        throw new Error();
+      }
+      //return new Response(JSON.stringify(importResults), { status: 200 });
+      return new Response('Success', { status: 200 });
+    } catch (err) {
+      return new Response(JSON.stringify({ message: 'Failed to fetch concepts' }), { status: 500 });
+    }
+  });
+};
+
+export const GET = async (req: NextRequest,
+                             props: { params: Promise<{ catalogId: string, resultId: string }> }) => {
+  const params = await props.params;
+  return await withValidSessionForApi(async (session) => {
+    const { catalogId, resultId } = params;
+
+    try {
+      const response = await getConceptImportResultById(catalogId, resultId, session?.accessToken);
+      if (response.status !== 200) {
+        throw new Error();
+      }
+
+      const jsonResponse = await response.json();
+      //return new Response(JSON.stringify(importResults), { status: 200 });
+      return new Response(JSON.stringify(jsonResponse), { status: 200 });
+    } catch (err) {
+      return new Response(JSON.stringify({ message: 'Failed to fetch concepts' }), { status: 500 });
+    }
+  });
+};
