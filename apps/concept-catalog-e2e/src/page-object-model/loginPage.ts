8899c1b2ca5867f37706bad9f0bc2a074021f007
Ahmed Abdelgawwad <ahmed.abdulgawad91@gmail.com>
2025-09-26 14:04:21 +0200

    Revert "feat: increase timeout for logging in ID-Porten"
    This reverts commit 1839ea4f8efa41f541f492f98155ee75b714cb91.

--- apps/concept-catalog-e2e/src/page-object-model/loginPage.ts
+++ apps/concept-catalog-e2e/src/page-object-model/loginPage.ts
@@ -37,7 +37,7 @@ export default class LoginPage {
     }
 
     console.log('[LOGIN PAGE] Clicking "Logg inn via ID-porten"...');
-    await this.page.getByRole('link', { name: 'Logg inn via ID-porten' }).click({ timeout: 20000 });
+    await this.page.getByRole('link', { name: 'Logg inn via ID-porten' }).click({ timeout: 5000 });
     console.log('[LOGIN PAGE] Clicking "TestID på nivå høyt"...');
     await this.page.getByRole('link', { name: 'TestID på nivå høyt Lag din egen testbruker' }).click();
     console.log('[LOGIN PAGE] Filling synthetic personidentifikator...');
