2c71ee8e41cff4b0233706bbd1f94287270b7c88
erlingj <joherl10@gmail.com>
2025-10-23 10:46:27 +0200

    add common ts config with tsconfigRootDir

--- apps/dataset-catalog/eslint.config.mjs
+++ apps/dataset-catalog/eslint.config.mjs
@@ -1,51 +1,14 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
-import js from '@eslint/js';
-import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import pluginReact from 'eslint-plugin-react';
-import pluginNx from '@nx/eslint-plugin';
-import tseslint from 'typescript-eslint';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createCommonTsConfig } from '../../tools/eslint/shared-eslint-config.mjs';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig([
   globalIgnores(['.next/**/*', 'node_modules/**/*']),
+  createCommonTsConfig(__dirname),
 
-  {
-    files: ['**/*.{js,cjs,mjs,ts,tsx,jsx}'],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.jest,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      react: pluginReact,
-      '@nx': pluginNx,
-    },
-    rules: {
-      ...tseslint.configs.recommended[0].rules,
-      ...pluginReact.configs.flat.recommended.rules,
-
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/triple-slash-reference': 'off',
-      '@next/next/no-html-link-for-pages': ['error', 'apps/dataset-catalog/pages'],
-    },
-  },
   {
     ignores: ['.next', '.next/**/*', '**/.next/**/*', 'node_modules/**/*'],
   },
