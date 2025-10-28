f1d9cfeb7305827ef640d5e8745e1ccecf87aae4
erlingj <joherl10@gmail.com>
2025-10-28 11:47:12 +0100

    update eslint

--- tools/eslint/shared-eslint-config.mjs
+++ tools/eslint/shared-eslint-config.mjs
@@ -3,16 +3,10 @@ import pluginReact from 'eslint-plugin-react';
 import pluginNx from '@nx/eslint-plugin';
 import tseslint from 'typescript-eslint';
 
-export function createCommonTsConfig(tsconfigRootDir, allow) {
-  const allowList = ['tools', ...(allow || [])];
-
+export function createCommonTsConfig(tsconfigRootDir) {
   return {
     files: ['**/*.{js,cjs,mjs,ts,tsx,jsx}'],
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
+    ignores: ['.next/**/*', 'node_modules/**/*'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
@@ -26,30 +20,27 @@ export function createCommonTsConfig(tsconfigRootDir, allow) {
         ...globals.jest,
       },
     },
+    rules: {
+      ...tseslint.configs.recommended[0].rules,
+      ...pluginReact.configs.flat.recommended.rules,
+
+      // not needed with Next.js / TS
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      // prefer TS-aware unused-vars
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
     plugins: {
       '@typescript-eslint': tseslint.plugin,
       react: pluginReact,
       '@nx': pluginNx,
     },
-    rules: {
-      ...tseslint.configs.recommended[0].rules,
-      ...pluginReact.configs.flat.recommended.rules,
-      'react/react-in-jsx-scope': 'off', // not needed with Next.js
-      'react/prop-types': 'off', // we use TypeScript for type checking
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          enforceBuildableLibDependency: true,
-          allow: allowList,
-          depConstraints: [
-            {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
-            },
-          ],
-        },
-      ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   };
 }
