ab5eb5f14c0a3edf57cf654465ad5f877a1f881c
erlingj <joherl10@gmail.com>
2025-12-16 14:10:35 +0100

    move getAdmsStatuses

--- libs/data-access/src/lib/reference-data/api/index.ts
+++ libs/data-access/src/lib/reference-data/api/index.ts
@@ -10,6 +10,20 @@ import {
   SearchQueryVariables,
 } from "../generated/graphql";
 
+export const getAdmsStatuses = async (): Promise<{
+  statuses: ReferenceDataCode[];
+}> => {
+  const path = `${process.env.FDK_BASE_URI}/reference-data/adms/statuses`;
+  const options = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "GET",
+  };
+  const response = await fetch(path, options);
+  return response.json();
+};
+
 export const getConceptStatuses = async (): Promise<{
   conceptStatuses: ReferenceDataCode[];
 }> => {
