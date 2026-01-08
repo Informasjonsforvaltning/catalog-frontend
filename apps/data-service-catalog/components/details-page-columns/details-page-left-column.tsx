6c13fd72b99a5315d0016b6affa8d2c3aeccf78a
Øyvind Julsrud <oyvind.julsrud@digdir.no>
2026-01-08 13:56:42 +0100

    fix: hide keywords section when all values are empty

--- apps/data-service-catalog/components/details-page-columns/details-page-left-column.tsx
+++ apps/data-service-catalog/components/details-page-columns/details-page-left-column.tsx
@@ -1,7 +1,7 @@
 import { DataService, DataServiceReferenceData } from "@catalog-frontend/types";
 import styles from "./details-columns.module.css";
 import { InfoCard } from "@catalog-frontend/ui";
-import { isEmpty } from "lodash";
+import { isEmpty, some } from "lodash";
 import { getTranslateText, localization } from "@catalog-frontend/utils";
 import { Paragraph, Tag } from "@digdir/designsystemet-react";
 import { DetailsUrlList } from "./components/details-url-list";
@@ -109,7 +109,7 @@ export const LeftColumn = ({
         </InfoCard.Item>
       )}
 
-      {!isEmpty(dataService?.keywords) && (
+      {some(dataService?.keywords, (v) => !isEmpty(v)) && (
         <InfoCard.Item
           title={localization.dataServiceForm.fieldLabel.keywords}
           data-testid="data-service-keywords"
