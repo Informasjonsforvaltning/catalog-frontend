ce443d96f8bef7f414931cb442055c63401c0db1
Øyvind Julsrud <oyvind.julsrud@digdir.no>
2026-01-08 13:22:08 +0100

    fix: remove empty language fields from data-service form after save

--- apps/data-service-catalog/components/data-service-form/utils/data-service-initial-values.tsx
+++ apps/data-service-catalog/components/data-service-form/utils/data-service-initial-values.tsx
@@ -1,4 +1,21 @@
-import { DataServiceToBeCreated } from "@catalog-frontend/types";
+import { DataService, DataServiceToBeCreated } from "@catalog-frontend/types";
+import { removeEmptyOrNullValues } from "@catalog-frontend/utils";
+
+export const dataServiceTemplate = (dataService: DataService): DataService => {
+  return {
+    ...dataService,
+    title: removeEmptyOrNullValues(dataService?.title),
+    description: removeEmptyOrNullValues(dataService?.description),
+    keywords: removeEmptyOrNullValues(dataService?.keywords),
+    contactPoint: {
+      ...dataService?.contactPoint,
+      name: removeEmptyOrNullValues(dataService?.contactPoint?.name),
+      email: dataService?.contactPoint?.email || undefined,
+      phone: dataService?.contactPoint?.phone || undefined,
+      url: dataService?.contactPoint?.url || undefined,
+    },
+  };
+};
 
 export const dataServiceToBeCreatedTemplate = (): DataServiceToBeCreated => {
   return {
