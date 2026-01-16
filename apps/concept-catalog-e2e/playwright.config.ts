4731c643cc827b6eac5e76481e9d5944ab2372a7
Øyvind Julsrud <oyvind.julsrud@digdir.no>
2026-01-16 09:54:50 +0100

    fix: add yarn command back

--- apps/concept-catalog-e2e/playwright.config.ts
+++ apps/concept-catalog-e2e/playwright.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "nx serve concept-catalog --configuration=e2e",
+    command: "yarn nx serve concept-catalog --configuration=e2e",
     url: "http://127.0.0.1:4200",
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
