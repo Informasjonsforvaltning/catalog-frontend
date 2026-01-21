0f6411b00ef510d83bb6748345a02cdda7b7fdef
erlingj <joherl10@gmail.com>
2026-01-21 14:19:24 +0100

    filter empty relatertBegrep

--- apps/concept-catalog/components/concept-form/components/relation-section.tsx
+++ apps/concept-catalog/components/concept-form/components/relation-section.tsx
@@ -133,7 +133,7 @@ export const RelationSection = ({
         filters: {
           uri: {
             value: relations
-              .filter((rel) => !rel.internal)
+              .filter((rel) => !rel.internal && rel.relatertBegrep)
               .map((rel) => rel.relatertBegrep as string),
           },
         },
