8fcdb7fd1a4801d3425802631d7dd60bbe3621a6
Øyvind Julsrud <oyvind.julsrud@digdir.no>
2026-01-22 12:57:17 +0100

    fix: add auth session modal to data-service-catalog

---
+++ apps/data-service-catalog/app/api/auth/validate/route.ts
@@ -0,0 +1,11 @@
+import { authOptions, isValidSessionAndToken } from "@catalog-frontend/utils";
+import { getServerSession } from "next-auth";
+import { NextRequest } from "next/server";
+
+const handler = async (_req: NextRequest) => {
+  const session = await getServerSession(authOptions);
+  const isValid = await isValidSessionAndToken(session);
+  return new Response(null, { status: isValid ? 200 : 401 });
+};
+
+export { handler as GET };
