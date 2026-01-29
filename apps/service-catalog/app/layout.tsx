91a3ea1105716eaef0a76ee2bdae27ccad5db4f0
Øyvind Julsrud <oyvjul@gmail.com>
2026-01-29 10:01:10 +0100

    fix: pass session to NextAuthProvider

--- apps/service-catalog/app/layout.tsx
+++ apps/service-catalog/app/layout.tsx
@@ -3,19 +3,22 @@ import {
   NextAuthProvider,
   ReactQueryClientProvider,
 } from "@catalog-frontend/ui";
-import { localization } from "@catalog-frontend/utils";
+import { authOptions, localization } from "@catalog-frontend/utils";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 
 export const metadata: Metadata = {
   title: localization.catalogType.service,
   description: localization.catalogType.service,
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang={localization.getLanguage()}>
       <body>
-        <NextAuthProvider>
+        <NextAuthProvider session={session}>
           <AuthSessionModal storageKey="serviceForm" />
           <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
         </NextAuthProvider>
