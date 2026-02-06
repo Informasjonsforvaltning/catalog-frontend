56fed8c97d6a55eaa3414a9a884a57a50200fec9
Øyvind Julsrud <oyvjul@gmail.com>
2026-02-06 08:40:01 +0100

    docs: link pagination issue in TODO comment

--- apps/concept-catalog/app/catalogs/[catalogId]/activity-log/comment-activity-log-content.tsx
+++ apps/concept-catalog/app/catalogs/[catalogId]/activity-log/comment-activity-log-content.tsx
@@ -11,6 +11,7 @@ export const CommentActivityLogContent = async ({
   accessToken,
 }: Props) => {
   const { comments } = await getCommentActivityLogData(catalogId, accessToken);
+  /* TODO Add pagination https://github.com/Informasjonsforvaltning/catalog-frontend/issues/1660 */
 
   return <CommentActivityLog catalogId={catalogId} comments={comments} />;
 };
