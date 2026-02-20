9ab2a104139ff581752a0e9dbd7f03d566ba6ab9
Øyvind Julsrud <oyvjul@gmail.com>
2026-02-20 09:05:08 +0100

    style: add spacing to auth-session-modal (#1727)

--- libs/ui-v2/src/lib/auth-session-modal/index.tsx
+++ libs/ui-v2/src/lib/auth-session-modal/index.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { Button, Dialog, Paragraph } from "@digdir/designsystemet-react";
+import {
+  Button,
+  Dialog,
+  Heading,
+  Paragraph,
+} from "@digdir/designsystemet-react";
 import {
   LocalDataStorage,
   localization,
@@ -9,6 +14,7 @@ import {
 } from "@catalog-frontend/utils";
 import { useRouter, usePathname } from "next/navigation";
 import { DialogActions } from "@catalog-frontend/ui-v2";
+import styles from "./auth-session-modal.module.css";
 
 type AuthSessionModalProps = {
   signInPath?: string;
@@ -61,28 +67,22 @@ export const AuthSessionModal = ({
 
   return (
     <Dialog ref={modalRef}>
-      <Dialog.Block>{localization.auth.sessionExpiredTitle}</Dialog.Block>
-      <Dialog.Block>
-        <Paragraph data-size="sm">
-          {hasStorageData
-            ? localization.auth.sessionExpiredWithStorage
-            : localization.auth.sessionExpired}
-        </Paragraph>
-      </Dialog.Block>
-      <Dialog.Block>
-        <DialogActions>
-          <Button data-size="sm" onClick={handleLoginClick}>
-            {localization.auth.login}
-          </Button>
-          <Button
-            data-size="sm"
-            variant="secondary"
-            onClick={handleCancelClick}
-          >
-            {localization.button.cancel}
-          </Button>
-        </DialogActions>
-      </Dialog.Block>
+      <Heading className={styles.heading} data-size="xs">
+        {localization.auth.sessionExpiredTitle}
+      </Heading>
+      <Paragraph data-size="sm" className={styles.paragraph}>
+        {hasStorageData
+          ? localization.auth.sessionExpiredWithStorage
+          : localization.auth.sessionExpired}
+      </Paragraph>
+      <DialogActions>
+        <Button data-size="sm" onClick={handleLoginClick}>
+          {localization.auth.login}
+        </Button>
+        <Button data-size="sm" variant="secondary" onClick={handleCancelClick}>
+          {localization.button.cancel}
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 };
